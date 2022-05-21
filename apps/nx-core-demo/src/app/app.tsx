e8d323706553b344d3ff5bbb13c00c19e8975b3d
Jarosenb <jrosenberg@tacc.utexas.edu>
2022-05-21 13:52:46 -0500

    initial commit

---
+++ apps/nx-core-demo/src/app/app.tsx
@@ -0,0 +1,14 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import styles from './app.module.scss';
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <>
+      <NxWelcome title="nx-core-demo" />
+      <div />
+    </>
+  );
+}
+
+export default App;
