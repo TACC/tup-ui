4df6525e519c8a0352a6a85b04e090f0a022127d
Wesley Bomar <wbomar@tacc.utexas.edu>
2022-06-03 22:21:08 -0500

    docs(tup-ui): comment to make a diff

--- apps/tup-ui/src/App.spec.tsx
+++ apps/tup-ui/src/App.spec.tsx
@@ -1,6 +1,6 @@
 import { render } from '@testing-library/react';
 
-import App from './app';
+import App from './app'; // test this
 
 describe('App', () => {
   it('should render successfully', () => {
