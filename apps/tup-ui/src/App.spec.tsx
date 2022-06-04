0e9ad52439d21ae58a947484ae797849c2dd0915
Wesley Bomar <wbomar@tacc.utexas.edu>
2022-06-03 22:23:14 -0500

    fix(tup-ui): (wip) attempted fix to test case

--- apps/tup-ui/src/App.spec.tsx
+++ apps/tup-ui/src/App.spec.tsx
@@ -1,6 +1,6 @@
 import { render } from '@testing-library/react';
 
-import App from './app'; // test this
+import App from './App';
 
 describe('App', () => {
   it('should render successfully', () => {
