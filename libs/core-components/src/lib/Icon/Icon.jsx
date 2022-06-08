af596ef230c9862bab6ba3b45a6941bb0f06a620
Wesley Bomar <wbomar@tacc.utexas.edu>
2022-06-08 16:04:01 -0500

    feat(comp…s): cepv2 update: button tests, unmerged
    Source: https://github.com/TACC/Core-Portal/pull/640

--- libs/core-components/src/lib/Icon/Icon.jsx
+++ libs/core-components/src/lib/Icon/Icon.jsx
@@ -1,42 +1,38 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import './Icon.module.css';
 
-import '@tacc/core-styles/source/_imports/components/cortal.icon.css';
-
-/**
- * An icon that supports:
- *
- * - name of a "Cortal" icon
- * - extra class names
- * - an ARIA label (passed as child content)
- *
- * __Notice__: Using this component adds global CSS for `.icon` and `.icon-*`.
- *
- * @example
- * // returns <i class="icon icon-allocations"></i>
- * <Icon name="allocations"></i>
- * @example
- * // returns <i class="icon icon-allocations" aria-label="trash-bin"></i>
- * <Icon name="trash">trash bin</i>
- */
-const Icon = ({ children, className, name }) => {
+const Icon = ({ children, className, dataTestid, name }) => {
   const iconClassName = `icon icon-${name}`;
-  const fullClassName = [className, iconClassName].join(' ').trim();
+  // FAQ: The conditional avoids an extra space in class attribute value
+  const fullClassName = className
+    ? [className, iconClassName].join(' ')
+    : iconClassName;
   const label = children;
 
-  return <i className={fullClassName} role="img" aria-label={label} />;
+  return (
+    <i
+      className={fullClassName}
+      role="img"
+      aria-label={label}
+      data-testid={dataTestid}
+    />
+  );
 };
 Icon.propTypes = {
   /** A text alternative to the icon (for accessibility) */
   children: PropTypes.string,
   /** Additional className for the root element */
   className: PropTypes.string,
+  /** ID for test case element selection */
+  dataTestid: PropTypes.string,
   /** Name of icon from icon font (without the (`icon-` prefix) */
   name: PropTypes.string.isRequired,
 };
 Icon.defaultProps = {
   children: '',
   className: '',
+  dataTestid: '',
 };
 
 export default Icon;
