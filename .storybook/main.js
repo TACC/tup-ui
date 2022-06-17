53598759bf198fdaa94347ef03868e2ffefbfc88
Wesley Bomar <wbomar@tacc.utexas.edu>
2022-06-17 10:52:24 -0500

    fix(core-components): (wip) esoteric build bugs

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,7 @@
 module.exports = {
   stories: [],
-  addons: ['@storybook/addon-essentials'],
+  // SEE: https://github.com/storybookjs/storybook/issues/17996#issuecomment-1134810729
+  addons: ['@storybook/addon-actions', '@storybook/addon-essentials'],
   // uncomment the property below if you want to apply some webpack config globally
   // webpackFinal: async (config, { configType }) => {
   //   // Make whatever fine-grained changes you need that should apply to all storybook configs
