e859114156c5bca3c25dbf952538856fac1d0240
Wesley Bomar <wbomar@tacc.utexas.edu>
2022-06-20 14:47:32 -0500

    fix(ui-patterns): activeClassN… react-r…-dom ver.
    Use same react-router-dom version as CEPv2 to make activeClassName work.
    - downgrade react-router-dom
    - use switch and component props

--- apps/ui-patterns/src/app/App.tsx
+++ apps/ui-patterns/src/app/App.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import UIPatterns from './UIPatterns';
 
 const App: React.FC = () => {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<UIPatterns />} />
-      </Routes>
+      <Switch>
+        <Route path="/" component={UIPatterns} />
+      </Switch>
     </Router>
   );
 };
