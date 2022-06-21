a858757f711c03c70d15e147b49ba26f1df56703
Joon-Yee Chuah <jchuah@tacc.utexas.edu>
2022-06-21 09:38:38 -0500

    Port tapis-ui/_wrappers

---
+++ libs/core-wrappers/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'core-components',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
+    '^.+\\.[tj]sx?$': 'babel-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/apps/tup-ui',
+};
---
+++ libs/core-wrappers/src/index.ts
@@ -0,0 +1,10 @@
+export { Navbar, NavItem } from './lib/Navbar';
+export { default as QueryWrapper } from './lib/QueryWrapper';
+export { default as SubmitWrapper } from './lib/SubmitWrapper';
+export { default as Wizard } from './lib/Wizard';
+export {
+  FieldWrapperFormik,
+  FormikInput,
+  FormikSelect,
+  FormikCheck,
+} from './lib/FieldWrapperFormik';
