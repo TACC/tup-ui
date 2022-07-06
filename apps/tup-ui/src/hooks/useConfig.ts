a62d820a3a8174634587e23f30641096f287099d
Joon-Yee Chuah <jchuah@tacc.utexas.edu>
2022-07-06 10:38:25 -0500

    Remove baseUrl warning

--- apps/tup-ui/src/hooks/useConfig.ts
+++ apps/tup-ui/src/hooks/useConfig.ts
@@ -3,9 +3,6 @@
 // Get config provided to the window object via SSR.
 const useConfig = () => {
   const config = window.__TUP_CONFIG__;
-  if (!config) {
-    console.warn("No baseUrl has been set, defaulting to http://localhost:8000");
-  }
   // return a default if no config exists on the window.
   return config ?? { baseUrl: 'http://localhost:8000' };
 };
