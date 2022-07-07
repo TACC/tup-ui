3c0cbefdaf82b337f33cbf5a83b93d41fde7d9bc
Joon-Yee Chuah <jchuah@tacc.utexas.edu>
2022-07-07 12:29:10 -0500

    test usePost

--- apps/tup-ui/src/hooks/requests.spec.tsx
+++ apps/tup-ui/src/hooks/requests.spec.tsx
@@ -1,8 +1,9 @@
+import React, { useEffect } from 'react';
 import { render, screen } from '@testing-library/react';
 import { AxiosStatic } from 'axios';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { AxiosClientContext} from './useAxios';
-import { useGet } from './requests';
+import { useGet, usePost } from './requests';
 import useJwt from './useJwt';
 
 jest.mock('./useJwt');
@@ -23,13 +24,30 @@ const testQueryClient = new QueryClient({
   defaultOptions: { queries: { retry: false, cacheTime: Infinity } },
 });
 
-const WrappedGet = () => (
+const Wrapper: React.FC<React.PropsWithChildren<{}>> = ({ children }) => (
   <AxiosClientContext.Provider value={mockedAxios}>
     <QueryClientProvider client={testQueryClient}>
-      <MockGetComponent />
+      {children}
     </QueryClientProvider>
   </AxiosClientContext.Provider>
 );
+
+const MockPostComponent: React.FC = () => {
+  const { data, isLoading, mutate, error } = usePost<string, string>("/endpoint");
+  useEffect(
+    () => {
+      mutate("body")
+    },
+    [ mutate ]
+  )
+  if (error) {
+    return <div>Error</div>
+  }
+  if (isLoading) {
+    return <div>Loading</div>
+  }
+  return <div>{data}</div>
+}
 
 describe('requests', () => {
   afterEach(() => testQueryClient.clear());
@@ -43,10 +61,26 @@ describe('requests', () => {
       .spyOn(mockedAxios, 'get')
       .mockResolvedValue({ data: "response" });
 
-    render(<WrappedGet />);
+    render(<Wrapper><MockGetComponent/></Wrapper>);
 
     const responseQuery = await screen.findByText(/response/);
     expect(responseQuery).toBeTruthy();
     expect(mockedAxios.get).toHaveBeenCalledWith('http://localhost:8000/endpoint', { headers: {'x-tup-token': 'abc123'}})
+  });
+  it('should render the mock post component', async () => {
+    (useJwt as jest.Mock).mockReturnValue({
+      jwt: "abc123",
+      isLoading: false,
+      error: null
+    })
+    jest
+      .spyOn(mockedAxios, 'post')
+      .mockResolvedValue({ data: "response" });
+
+    render(<Wrapper><MockPostComponent/></Wrapper>);
+
+    const responseQuery = await screen.findByText(/response/);
+    expect(responseQuery).toBeTruthy();
+    expect(mockedAxios.post).toHaveBeenCalledWith('http://localhost:8000/endpoint', "body", { headers: {'x-tup-token': 'abc123'}})
   });
 });
