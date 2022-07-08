f01aa159a710e040798f3d0b069b020b1c6bf6a1
Joon-Yee Chuah <jchuah@tacc.utexas.edu>
2022-07-08 14:18:50 -0500

    Update comment

--- apps/tup-ui/src/mocks/handlers.ts
+++ apps/tup-ui/src/mocks/handlers.ts
@@ -3,8 +3,7 @@ import { mockProfile } from './fixtures';
 
 export const handlers = [
   rest.get('http://localhost:8000/auth/profile', (req, res, ctx) => {
-    // Respond with a mocked user token that gets persisted
-    // in the `sessionStorage` by the `Login` component.
+    // Respond with a TAS user profile
     return res(ctx.json(mockProfile));
   }),
 ];
