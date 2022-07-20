3f1c2117e00fff3295bbe74fb0ca535dd925ecb9
Jarosenb <jrosenberg@tacc.utexas.edu>
2022-07-20 09:37:01 -0500

    Remove unused code.

--- apps/tup-ui/src/App.tsx
+++ apps/tup-ui/src/App.tsx
@@ -59,23 +59,6 @@ function App() {
       </Route>
     </Routes>
   );
-
-  const [count, setCount] = useState(0);
-
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>Hello Vite + React!</p>
-        <Message />
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <LoginComponent />
-      </header>
-    </div>
-  );
 }
 
 export default App;
