b5efe9c813fd13b29e126807a89776293d15a79f
Joon-Yee Chuah <jchuah@tacc.utexas.edu>
2022-07-26 12:14:17 -0500

    Login component styling

--- apps/tup-ui/src/components/auth/LoginComponent/LoginComponent.tsx
+++ apps/tup-ui/src/components/auth/LoginComponent/LoginComponent.tsx
@@ -1,7 +1,40 @@
-import React, { useCallback, useState } from 'react';
+import React, { useCallback } from 'react';
 import { useLocation, Location, useNavigate } from 'react-router-dom';
 import { useAuth } from '@tacc/tup-ui/hooks';
+import { Formik, Form, Field, useFormik, useFormikContext } from 'formik';
+import { Label } from 'reactstrap';
+import { Button } from '@tacc/core-components';
+import * as Yup from 'yup';
 import styles from './LoginComponent.module.css';
+import logo from '@tacc/tup-ui/assets/TACC-formal-Black-1c.svg';
+
+type LoginInfo = {
+  username: string;
+  password: string;
+};
+
+type LoginFieldProps = {
+  name: string;
+  label: string;
+  type?: string;
+};
+
+const LoginField: React.FC<LoginFieldProps> = ({ name, label, type }) => {
+  // Matt does not want these form stylings to be global
+  return (
+    <div className={styles['form-field']}>
+      <Label
+        className={styles['form-field__label']}
+        size="sm"
+        style={{ display: 'flex', alignItems: 'center' }}
+        htmlFor={name}
+      >
+        {label}
+      </Label>
+      <Field name={name} id={name} type={type} />
+    </div>
+  );
+};
 
 const LoginComponent: React.FC = () => {
   const navigate = useNavigate();
@@ -13,40 +46,54 @@ const LoginComponent: React.FC = () => {
   );
   const { login, error, isLoading } = useAuth();
 
-  const [password, setPassword] = useState('');
-  const [username, setUsername] = useState('');
+  const validationSchema = Yup.object({
+    username: Yup.string().required(),
+    password: Yup.string().required(),
+  });
 
-  const authenticate = (e: React.FormEvent) => {
-    e.preventDefault();
-    login({ username, password }, { onSuccess: authCallback });
+  const initialValues: LoginInfo = {
+    username: '',
+    password: '',
   };
+
+  const onSubmit = useCallback(
+    ({ username, password }: LoginInfo) => {
+      console.log(username, password);
+      //login({ username, password }, { onSuccess: authCallback });
+    },
+    [login]
+  );
 
   if (isLoading) {
     return <div>...</div>;
   }
 
   return (
-    <div>
-      <form onSubmit={authenticate}>
-        <div>Login:</div>
-        <div>
-          Username:{' '}
-          <input
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-          />
-        </div>
-        <div>
-          Password:{' '}
-          <input
-            type={'password'}
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </div>
-        <button type="submit">Log in</button>
-        {error ? <div>Error logging in </div> : null}
-      </form>
+    <div className={styles.root}>
+      <img src={logo} className={styles.logo} />
+      <h2>Log In</h2>
+      <div className={styles.subtitle}>to continue to the TACC User Portal</div>
+      <Formik
+        initialValues={initialValues}
+        validationSchema={validationSchema}
+        onSubmit={onSubmit}
+      >
+        <Form className={styles.form}>
+          <LoginField name="username" label="User Name" />
+          <LoginField name="password" label="Password" type="password" />
+          <div className={styles['submit-container']}>
+            <div>
+              Create Account
+            </div>
+            <Button type="primary" attr="submit" size="long" className={styles.submit}>
+              Log In
+            </Button>
+          </div>
+        </Form>
+      </Formik>
+      
+        <div className={styles.help}>Having trouble logging in?</div>
+        <div className={styles.help}>Account Help</div>
     </div>
   );
 };
