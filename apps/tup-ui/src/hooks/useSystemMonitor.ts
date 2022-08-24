be6b9275696f6e93da6acbfc6883950c3101c9f9
Joon-Yee Chuah <jchuah@tacc.utexas.edu>
2022-08-24 11:19:20 -0500

    Comment about alternate baseUrl

--- apps/tup-ui/src/hooks/useSystemMonitor.ts
+++ apps/tup-ui/src/hooks/useSystemMonitor.ts
@@ -58,6 +58,7 @@ const useSystemMonitor = (
   const query = useGet<SystemMonitorRaw>({
     endpoint: '/sysmon',
     key: 'sysmon',
+    // alternate baseUrl until https://github.com/TACC/tup-services/pull/13 (system monitor nginx) has been deployed
     baseUrl: 'http://localhost',
   });
   const { data } = query;
