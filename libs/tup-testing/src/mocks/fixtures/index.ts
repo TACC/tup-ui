d6f436e1241c22ba6051b289dff515dd52ae7c07
Sophia Massie <smmassie92@gmail.com>
2022-11-17 11:45:00 -0600

    Format check updates

--- libs/tup-testing/src/mocks/fixtures/index.ts
+++ libs/tup-testing/src/mocks/fixtures/index.ts
@@ -1,8 +1,5 @@
 export { default as mockProfile } from './profile';
 export { default as mockJwt } from './auth';
-<<<<<<< Updated upstream
-export { rawSystemMonitorOutput } from './system_monitor';
-export { mockTickets } from './tickets';
-=======
 export { default as rawSystemMonitorOutput } from './system_monitor';
->>>>>>> Stashed changes
+export { mockTickets } from './tickets';
+
