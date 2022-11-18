45e3a1fc4ce1810353fdb018fb315d06f7b5df32
Sophia Massie <smmassie92@gmail.com>
2022-11-18 11:36:31 -0600

    Need to update system name test for sysmon table

--- libs/tup-components/src/system_monitor/SystemMonitor.test.tsx
+++ libs/tup-components/src/system_monitor/SystemMonitor.test.tsx
@@ -1,17 +1,37 @@
 import React from 'react';
 import SystemMonitor from './SystemMonitor';
-import { testRender } from '@tacc/tup-testing';
-import { waitFor } from '@testing-library/react';
+import { server, testRender } from '@tacc/tup-testing';
+import { getAllByRole, waitFor } from '@testing-library/react';
+import { rest } from 'msw';
+import { rawSystemMonitorOutput } from 'libs/tup-testing/src/mocks/fixtures';
 
 describe('System Monitor Component', () => {
-  it('display a no-systems message when there is no data', async () => {
-    const { getByText } = testRender(<SystemMonitor display_name={[]} />);
+  it('should render message when there are no systems to show', async () => {
+    server.use(
+      rest.get('http://localhost:8001/system_monitor', (req, res, ctx) => {
+        return res.once(ctx.json([]));
+      })
+      );
+      const { getByText } = testRender(<SystemMonitor />);
+      await waitFor(() =>
+        expect(getByText('No systems being monitored')).toBeDefined()
+      );
+    });
+  it('should display an error message if an error is returned from useQuery', async () => {
+    server.use(
+      rest.get('http://localhost:8001/system_monitor', (req, res, ctx) =>
+        res.once(ctx.status(404))
+      )
+    );
+    const { getByText } = testRender(<SystemMonitor />);
     await waitFor(() =>
-      expect(getByText('No systems being monitored')).toBeDefined()
+      expect(getByText('Unable to gather system information')).toBeDefined()
     );
   });
   it('should display the system name in each row', async () => {
     const { getByText } = testRender(<SystemMonitor />);
     await waitFor(() => expect(getByText('Frontera')).toBeDefined());
+    const {debug} = testRender(<SystemMonitor />);
+    debug();  
   });
 });
