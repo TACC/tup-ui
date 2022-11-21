1c06c93e1a9646180a0d4c24bf16562fefd2d1f9
Sophia Massie <smmassie92@gmail.com>
2022-11-21 15:46:46 -0600

    Updated working tests

--- libs/tup-testing/src/mocks/fixtures/system_monitor.ts
+++ libs/tup-testing/src/mocks/fixtures/system_monitor.ts
@@ -1,70 +1,103 @@
-export const rawSystemMonitorOutput = {
-  "Longhorn": {
-    "display_name": "Longhorn",
-    "tas_name": "longhorn3",
-    "hostname": "longhorn.tacc.utexas.edu",
-    "system_type": "COMPUTE",
-    "timestamp": "2022-11-07T14:30:02.709924+00:00",
-    "online": true,
-    "reachable": true,
-    "queues_down": false,
-    "load": 0.5865,
-    "running": 13,
-    "waiting": 6,
-    "in_maintenance": false,
-    "next_maintenance": null,
-    "reservations": []
-  },
-  "Stampede2": {
+export const rawSystemMonitorOutput = [
+  {
     "display_name": "Stampede2",
     "tas_name": "stampede4",
     "hostname": "stampede2.tacc.utexas.edu",
     "system_type": "COMPUTE",
-    "timestamp": "2022-11-07T14:30:06.059382+00:00",
+    "timestamp": "2022-11-21T21:35:03.189706+00:00",
     "online": true,
     "reachable": true,
     "queues_down": false,
-    "load": 0.5965,
-    "running": 412,
-    "waiting": 437,
+    "load": 0.83,
+    "running": 655,
+    "waiting": 196,
     "in_maintenance": false,
     "next_maintenance": null,
     "reservations": [
       {
         "name": "float_dev",
-        "begin_time": "2022-11-07T10:31:15-06:00",
-        "end_time": "2023-11-07T10:31:15-06:00"
+        "begin_time": "2022-11-21T17:36:14-06:00",
+        "end_time": "2023-11-21T17:36:14-06:00"
       }
     ]
   },
-  "Maverick2": {
+  {
+    "display_name": "Longhorn",
+    "tas_name": "longhorn3",
+    "hostname": "longhorn.tacc.utexas.edu",
+    "system_type": "COMPUTE",
+    "timestamp": "2022-11-21T21:35:14.019409+00:00",
+    "online": true,
+    "reachable": true,
+    "queues_down": false,
+    "load": 0.625,
+    "running": 18,
+    "waiting": 0,
+    "in_maintenance": false,
+    "next_maintenance": null,
+    "reservations": [
+      {
+        "name": "Retirement-2022-11-24",
+        "begin_time": "2022-11-24T09:00:00-06:00",
+        "end_time": "2023-02-01T19:39:00-06:00"
+      }
+    ]
+  },
+  {
     "display_name": "Maverick2",
     "tas_name": "maverick3",
     "hostname": "maverick2.tacc.utexas.edu",
     "system_type": "COMPUTE",
-    "timestamp": "2022-11-07T14:30:15.071987+00:00",
+    "timestamp": "2022-11-21T21:35:21.348912+00:00",
     "online": true,
     "reachable": true,
     "queues_down": false,
-    "load": 0.2,
-    "running": 6,
+    "load": 0.0667,
+    "running": 2,
     "waiting": 0,
     "in_maintenance": false,
     "next_maintenance": null,
     "reservations": []
   },
-  "Frontera": {
+  {
+    "display_name": "Lonestar6",
+    "tas_name": "lonestar6",
+    "hostname": "ls6.tacc.utexas.edu",
+    "system_type": "COMPUTE",
+    "timestamp": "2022-11-21T21:35:17.411427+00:00",
+    "online": true,
+    "reachable": true,
+    "queues_down": false,
+    "load": 0.8093,
+    "running": 243,
+    "waiting": 0,
+    "in_maintenance": false,
+    "next_maintenance": null,
+    "reservations": [
+      {
+        "name": "additional_gpus_testing",
+        "begin_time": "2022-11-07T16:58:34-06:00",
+        "end_time": "2022-12-31T00:00:00-06:00"
+      },
+      {
+        "name": "MPI_testing",
+        "begin_time": "2022-11-16T11:13:00-06:00",
+        "end_time": "2022-12-31T00:00:00-06:00"
+      }
+    ]
+  },
+  {
     "display_name": "Frontera",
     "tas_name": "frontera",
     "hostname": "frontera.tacc.utexas.edu",
     "system_type": "COMPUTE",
-    "timestamp": "2022-11-07T14:30:21.414811+00:00",
+    "timestamp": "2022-11-21T21:35:27.647101+00:00",
     "online": true,
     "reachable": true,
-    "queues_down": false,
-    "load": 0.971,
-    "running": 365,
-    "waiting": 247,
+    "queues_down": true,
+    "load": 0.9437000000000001,
+    "running": 441,
+    "waiting": 191,
     "in_maintenance": false,
     "next_maintenance": null,
     "reservations": [
@@ -74,31 +107,10 @@ export const rawSystemMonitorOutput = {
         "end_time": "2024-01-25T12:00:00-06:00"
       },
       {
-        "name": "HPC_SI_day4",
-        "begin_time": "2022-11-10T11:00:00-06:00",
-        "end_time": "2022-11-10T17:30:00-06:00"
-      },
-      {
         "name": "float+dev",
-        "begin_time": "2022-11-07T10:30:26-06:00",
-        "end_time": "2023-11-07T10:30:26-06:00"
+        "begin_time": "2022-11-21T17:35:33-06:00",
+        "end_time": "2023-11-21T17:35:33-06:00"
       }
     ]
-  },
-  "Lonestar6": {
-    "display_name": "Lonestar6",
-    "tas_name": "lonestar6",
-    "hostname": "ls6.tacc.utexas.edu",
-    "system_type": "COMPUTE",
-    "timestamp": "2022-11-07T14:30:28.787394+00:00",
-    "online": true,
-    "reachable": true,
-    "queues_down": false,
-    "load": 0.6007,
-    "running": 161,
-    "waiting": 38,
-    "in_maintenance": false,
-    "next_maintenance": null,
-    "reservations": []
   }
-};
+]
