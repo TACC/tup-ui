2bfda2b8e3c55d819ce7e5bb1906ce680e88f289
Sophia Massie <smmassie92@gmail.com>
2022-11-22 08:55:26 -0600

    Added header

--- libs/tup-components/src/system_monitor/SystemMonitor.tsx
+++ libs/tup-components/src/system_monitor/SystemMonitor.tsx
@@ -1,6 +1,10 @@
 import React, { useMemo } from 'react';
 import { useTable, Column } from 'react-table';
-import { LoadingSpinner, InlineMessage } from '@tacc/core-components';
+import {
+  LoadingSpinner,
+  InlineMessage,
+  SectionTableWrapper,
+} from '@tacc/core-components';
 import { Display, Operational, Load } from './SystemMonitorCells';
 import { SystemMonitorRawSystem, useSystemMonitor } from '@tacc/tup-hooks';
 import styles from './SystemMonitor.module.css';
@@ -67,42 +71,44 @@ export const SystemMonitor: React.FC<{ display_name?: Array<string> }> = () => {
   }
 
   return (
-    <table
-      {...getTableProps()}
-      // Emulate <InfiniteScrollTable> and its use of `o-fixed-header-table`
-      // TODO: Create global table styles & Make <InfiniteScrollTable> use them
-      className={`multi-system InfiniteScrollTable o-fixed-header-table ${styles['root']}`}
-    >
-      <thead>
-        {headerGroups.map((headerGroup) => (
-          <tr
-            {...headerGroup.getHeaderGroupProps()}
-            className={styles['header']}
-          >
-            {headerGroup.headers.map((column) => (
-              <th key={column.id}>{column.render('Header')}</th>
-            ))}
-          </tr>
-        ))}
-      </thead>
-      <tbody {...getTableBodyProps()} className={styles['rows']}>
-        {rows.length ? (
-          rows.map((row, idx) => {
-            prepareRow(row);
-            return (
-              <tr {...row.getRowProps()}>
-                {row.cells.map((cell) => (
-                  <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
-                ))}
-              </tr>
-            );
-          })
-        ) : (
-          <tr>
-            <td colSpan={5}>No systems being monitored</td>
-          </tr>
-        )}
-      </tbody>
-    </table>
+    <SectionTableWrapper header="System Status">
+      <table
+        {...getTableProps()}
+        // Emulate <InfiniteScrollTable> and its use of `o-fixed-header-table`
+        // TODO: Create global table styles & Make <InfiniteScrollTable> use them
+        className={`multi-system InfiniteScrollTable o-fixed-header-table ${styles['root']}`}
+      >
+        <thead>
+          {headerGroups.map((headerGroup) => (
+            <tr
+              {...headerGroup.getHeaderGroupProps()}
+              className={styles['header']}
+            >
+              {headerGroup.headers.map((column) => (
+                <th key={column.id}>{column.render('Header')}</th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+        <tbody {...getTableBodyProps()} className={styles['rows']}>
+          {rows.length ? (
+            rows.map((row, idx) => {
+              prepareRow(row);
+              return (
+                <tr {...row.getRowProps()}>
+                  {row.cells.map((cell) => (
+                    <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+                  ))}
+                </tr>
+              );
+            })
+          ) : (
+            <tr>
+              <td colSpan={5}>No systems being monitored</td>
+            </tr>
+          )}
+        </tbody>
+      </table>
+    </SectionTableWrapper>
   );
 };
