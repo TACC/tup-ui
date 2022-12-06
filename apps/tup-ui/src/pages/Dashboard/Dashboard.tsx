f9fcb272d6e9d9d891ec9e28898abf97a598767b
sophia-massie <96220951+sophia-massie@users.noreply.github.com>
2022-12-06 09:31:28 -0600

    TUP 380 Create blank proj & allocations page (#75)
    * TUP 380 Create blank proj & allocations page
    
    * Removed unneccesary wrappers and classes from Proj

--- apps/tup-ui/src/pages/Dashboard/Dashboard.tsx
+++ apps/tup-ui/src/pages/Dashboard/Dashboard.tsx
@@ -1,7 +1,4 @@
 import React from 'react';
-//import { ProfileComponent } from '../../components/profile';
-//import { RequireAuth } from '../../components/utils';
-//import SystemMonitor from '../../components/sysmon';
 import {
   ProfileComponent,
   RequireAuth,
