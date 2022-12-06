78c3ef9af8706b56029f3e052d59434f12e7731d
Wesley Bomar <wbomar@tacc.utexas.edu>
2022-12-06 11:14:03 -0600

    fix(tup-cms): blog/news urls (i forgot this)

--- apps/tup-cms/src/taccsite_cms/urls_custom.py
+++ apps/tup-cms/src/taccsite_cms/urls_custom.py
@@ -1,6 +1,11 @@
 from django.urls import path, include
 
+from django.conf.urls import url
+
 custom_urls = [
     path('core/markup/nav/', include('apps.portal_nav.urls', namespace='portal_nav')),
     path('dashboard/', include('apps.dashboard.urls', namespace='dashboard')),
+
+    # Support `taggit_autosuggest` (from `djangocms-blog`)
+    url(r'^taggit_autosuggest/', include('taggit_autosuggest.urls')),
 ]
