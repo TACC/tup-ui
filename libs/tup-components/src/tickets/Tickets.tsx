a25dc0264dd738f253a317ee1deb1fbdc3101782
ryamase <ryamase@gmail.com>
2022-12-08 09:54:42 -0600

    bug fix: use RequireAuth in Tickets

--- libs/tup-components/src/tickets/Tickets.tsx
+++ libs/tup-components/src/tickets/Tickets.tsx
@@ -2,26 +2,29 @@ import React from 'react';
 import { Link, Outlet } from 'react-router-dom';
 import { SectionTableWrapper } from '@tacc/core-components';
 import { TicketsTable } from './TicketsTable';
+import { RequireAuth } from '../utils';
 
 const Tickets: React.FC = () => {
   return (
-    <>
-      <SectionTableWrapper
-        header="Tickets"
-        headerActions={
-          <Link
-            to={'create'}
-            className="c-button c-button--secondary c-button--size-small"
-          >
-            + New Ticket
-          </Link>
-        }
-        contentShouldScroll
-      >
-        <TicketsTable />
-      </SectionTableWrapper>
-      <Outlet />
-    </>
+    <RequireAuth>
+      <>
+        <SectionTableWrapper
+          header="Tickets"
+          headerActions={
+            <Link
+              to={'create'}
+              className="c-button c-button--secondary c-button--size-small"
+            >
+              + New Ticket
+            </Link>
+          }
+          contentShouldScroll
+        >
+          <TicketsTable />
+        </SectionTableWrapper>
+        <Outlet />
+      </>
+    </RequireAuth>
   );
 };
 
