427ca8b88ae1f50cff1401d90801506855915981
ryamase <ryamase@gmail.com>
2022-12-09 15:44:30 -0600

    update tests

--- libs/tup-components/src/tickets/Tickets.test.tsx
+++ libs/tup-components/src/tickets/Tickets.test.tsx
@@ -2,8 +2,14 @@ import React from 'react';
 import Tickets from './Tickets';
 import { testRender } from '@tacc/tup-testing';
 import { waitFor } from '@testing-library/react';
+import { get } from 'js-cookie';
+
+jest.mock('js-cookie');
 
 describe('Tickets Component', () => {
+  beforeEach(() => {
+    (get as jest.Mock).mockReturnValue('badjwt');
+  });
   it('should render SectionTableWrapper and Table components', async () => {
     const { getByText } = testRender(<Tickets />);
     await waitFor(() => expect(getByText('Subject')).toBeDefined());
--- libs/tup-components/src/tickets/TicketsTable.test.tsx
+++ libs/tup-components/src/tickets/TicketsTable.test.tsx
@@ -28,12 +28,10 @@ describe('Tickets Table Component', () => {
       })
     );
     const { getByText } = testRender(<TicketsTable />);
-    await waitFor(() =>
-      expect(getByText(/No tickets. You can add a ticket/)).toBeDefined()
-    );
-    expect(getByText('here').closest('a')?.getAttribute('href')).toEqual(
-      '/ticket-create'
-    );
+    await waitFor(() => expect(getByText(/No tickets. You can/)).toBeDefined());
+    expect(
+      getByText('add a ticket').closest('a')?.getAttribute('href')
+    ).toEqual('/ticket-create');
   });
   it('should display an error message if an error is returned from useQuery', async () => {
     server.use(
