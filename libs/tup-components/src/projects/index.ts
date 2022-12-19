d55394488f016332e3af607c72caf0d0963b32a1
sophia-massie <96220951+sophia-massie@users.noreply.github.com>
2022-12-19 16:34:50 -0600

    Task/tup 373 project summary listing (#86)
    * Need compute and storage objects
    
    * Correct format for SummaryListing
    
    * Format check
    
    * All pieces in place minimal markup to give space for UI
    
    * feat(core-styles): use remote v1.0, not local v0.7 (#70)
    
    * feat(core-styles): use remote not local
    
    * feat(core-styles): import all deps in each pattern
    
    * feat(core-styles): common path alias
    
    * feat(core-styles): libPaths → libAlias, etc.
    
    * feat(core-styles): update tup-cms path to theme
    
    * feat(core-styles): lib alias path → real dist dir
    
    * docs(ui-patterns): fix comment about core-styles
    
    * wip: css layers, forms, login
    
    * chore(core-styles): update font path
    
    Caused by: https://github.com/TACC/Core-Styles/pull/74
    
    * formatting
    
    * fix(fonts): path consistent w/ core-styles
    
    * fix(tup-ui): login form
    
    - changed to load latest core-cms image
    - changed input to allow optional required attribute
    - changed FieldWrapperFormik to use @tacc/core-styles c-form styles
    - changed LoginComponent to use @tacc/core-styles c-form
    
    TODO:
    - remove c-form classes from LoginComponent (this may mean creating more core-wrappers)
    
    * fix(ui-patterns): load missing styles
    
    Replace broken index.css with new main.css.
    
    * fix(ui-patterns): restore missing css vars
    
    Previously, I told postcss to convert css vars to static values. Why?
    
    Doesn't matter. Core-Styles serves settings very well now.
    
    * chore(core-styles): install latest & via cms
    
    - directly install latest core-styles
    - install core-cms image that has latest core-styles
    
    * chore(core-components): revive InfiniteScrollTable
    
    I don't need to convert this for this branch/PR to be comple.
    
    I will convert it just after.
    
    * chore(tup-components): comment out yup validation
    
    Use HTML5 validation, until told otherwise, for this form.
    
    * fix(core-components): Paginator use new c-button
    
    Update Paginator to rely on newest c-button styles.
    
    * chore: nx format:write
    
    * feat: add portal-specific assets, use new cms img
    
    * fix(core-wrappers): use core button not reactstrap
    
    * feat(ui-patterns): load core-styles, fix font size
    
    - Added relevant CSS from @tacc/core-styles.
    - Changed root font size to 10px.
    - Changed styles to retain original font sizes.
    - Removed Portal link styles.
    
    * fix(tup-u): restore loading spinner
    
    - Moved Bootstrap v5 classes from root to LoadingSpinner.
    - Renamed LoadingSpinner.css to LoadingSpinner.global.css.
    
    * fix(tup-ui): improve nav link legibility
    
    * fix(tup-ui): use c-button not bootstrap
    
    * fix(tup-ui): drop tiny, outdated, cepv2 font sizes
    
    * docs(tup-ui): how to style
    
    * chore: nx format:write
    
    * chore(tup-cms, core-styles): install latest
    
    * chore(tup-cms, core-styles): install latest-er
    
    * chore(core-styles): install bleeding edge
    
    * fix(core-styles): no CSS layers in source CSS
    
    - And thus, don't suggest it for ui-patterns.
    - Also, fix message color to use new core-styles color.
    
    * fix(tup-ui): global layout bugs
    
    - app: unset cms main margin
    - login: add missing padding
    - login: simply layout styling
    - main: stretch app height to available space
    - index: remove unused `o-site` class
    
    * fix(cms): demo fixes and new drop-cap pattern
    
    https://github.com/TACC/Core-Styles/pull/84
    
    https://github.com/TACC/Core-CMS/pull/569/commits/400868
    
    * chore(core-styles): sync to cms core-styles ver.
    
    Instead of pre-v1.0.0 commit, get a post-v1.0.0 commit.
    
    * fix(login): use large field inputs always
    
    This is not Core. Just specific to login form.
    
    Co-authored-by: jarosenb <jrosenberg@tacc.utexas.edu>
    
    * Summary listing component for all projects
    
    * refactor to standalone ProjectSummary component
    
    * Added tests
    
    * linting
    
    * linting
    
    Co-authored-by: jarosenb <jrosenberg@tacc.utexas.edu>

--- libs/tup-components/src/projects/index.ts
+++ libs/tup-components/src/projects/index.ts
@@ -1,3 +1,4 @@
 export { default as ProjectsTable } from './ProjectsTable';
 export { default as ProjectsDashboard } from './ProjectsDashboard';
 export { default as ProjectsLayout } from './ProjectsLayout';
+export { default as ProjectsSummaryListing } from './ProjectsSummaryListing';
