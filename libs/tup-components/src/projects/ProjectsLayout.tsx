bea83861260ceadc1140d539615ed26ef2397059
Van Go <35277477+van-go@users.noreply.github.com>
2022-12-21 11:22:48 -0600

    tup-374-changes to projects layout page

--- libs/tup-components/src/projects/ProjectsLayout.tsx
+++ libs/tup-components/src/projects/ProjectsLayout.tsx
@@ -1,13 +1,6 @@
 import React from 'react';
-import { PageLayout } from '../layout';
 import { Section } from '@tacc/core-components';
 import { AllocationsTable } from '../allocations';
-
-
-// const ProjectsLayout: React.FC = () => {
-//   return <Section header="Projects & Allocations" content={<PageLayout />} />;
-// };
-// export default ProjectsLayout;
 
 const ProjectsLayout: React.FC = () => {
   return (
