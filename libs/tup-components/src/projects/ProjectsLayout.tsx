974a30a16ef4f5d7ccd7e67fbc9da409c3ce0505
Van Go <35277477+van-go@users.noreply.github.com>
2023-01-02 15:55:15 -0600

    tup-374-format-write

--- libs/tup-components/src/projects/ProjectsLayout.tsx
+++ libs/tup-components/src/projects/ProjectsLayout.tsx
@@ -3,35 +3,35 @@ import { Section } from '@tacc/core-components';
 import { ProjectsSummaryListing } from '../projects';
 import { AllocationsTable } from '../allocations';
 
-const ProjectsLayout: React.FC = () => {
-  return (
-    <Section
-      header="Projects & Allocations"
-      content={
-        <table >
-          <tbody>
-            <td >
-              <ProjectsSummaryListing />
-              <AllocationsTable />
-            </td>
-          </tbody>
-        </table>
-      }
-    />
-  );
-};
-
 // const ProjectsLayout: React.FC = () => {
 //   return (
 //     <Section
 //       header="Projects & Allocations"
 //       content={
-//         <div>
-//           <ProjectsSummaryListing />
-//           <AllocationsTable />
-//         </div>
+//         <table >
+//           <tbody>
+//             <td >
+//               <ProjectsSummaryListing />
+//               <AllocationsTable />
+//             </td>
+//           </tbody>
+//         </table>
 //       }
 //     />
 //   );
 // };
+
+const ProjectsLayout: React.FC = () => {
+  return (
+    <Section
+      header="Projects & Allocations"
+      content={
+        <div>
+          <ProjectsSummaryListing />
+          <AllocationsTable />
+        </div>
+      }
+    />
+  );
+};
 export default ProjectsLayout;
