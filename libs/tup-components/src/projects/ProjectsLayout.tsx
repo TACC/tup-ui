f64db5bf78f5fa9bfa6c7d6abc3ab271edf9093e
Van Go <35277477+van-go@users.noreply.github.com>
2023-01-03 10:04:32 -0600

    tup-374-new-branch-commit

--- libs/tup-components/src/projects/ProjectsLayout.tsx
+++ libs/tup-components/src/projects/ProjectsLayout.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import { Section } from '@tacc/core-components';
 import { ProjectsSummaryListing } from './ProjectsSummaryListing';
+import { AllocationsTable } from '../allocations';
 
 const ProjectsLayout: React.FC = () => {
   return (
     <Section
       header="Projects & Allocations"
-      content={<ProjectsSummaryListing />}
+      content={
+      <tbody>
+        <ProjectsSummaryListing />
+        <AllocationsTable />
+      </tbody>
+    }
     />
   );
 };
