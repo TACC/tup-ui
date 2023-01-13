abb0050f5db1f6d42d23e8d0a3e1a4eefe2b1667
jarosenb <jrosenberg@tacc.utexas.edu>
2023-01-13 11:18:35 -0600

    demo of a callback running on django-cms form submission

--- apps/tup-cms/src/apps/dashboard/apps.py
+++ apps/tup-cms/src/apps/dashboard/apps.py
@@ -1,5 +1,17 @@
 from django.apps import AppConfig
+import logging
+from django.dispatch import receiver
+from djangocms_forms.signals import form_submission
 
+logger = logging.getLogger(f"portal.{__name__}")
+
+def callback(form, cleaned_data, **kwargs):
+    logger.debug(f"received submission from {form.name}")
+    logger.debug(cleaned_data)
 
 class DashboardConfig(AppConfig):
-    name = 'dashboard'
+    name = "apps.dashboard"
+
+    def ready(self):
+        form_submission.connect(callback)
+
--- apps/tup-cms/src/taccsite_cms/custom_app_settings.py
+++ apps/tup-cms/src/taccsite_cms/custom_app_settings.py
@@ -1,3 +1,3 @@
-CUSTOM_APPS = ['apps.portal_nav', 'apps.dashboard']
+CUSTOM_APPS = ['apps.portal_nav', 'apps.dashboard.apps.DashboardConfig']
 CUSTOM_MIDDLEWARE = []
 STATICFILES_DIRS = ()
