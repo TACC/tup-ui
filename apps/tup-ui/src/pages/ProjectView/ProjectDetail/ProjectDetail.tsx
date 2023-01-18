82f16daaf9c00ffe4d20339eca3ec0d94ac2df91
Van Go <35277477+van-go@users.noreply.github.com>
2023-01-18 10:42:57 -0600

    tup-376-format-write

--- apps/tup-ui/src/pages/ProjectView/ProjectDetail/ProjectDetail.tsx
+++ apps/tup-ui/src/pages/ProjectView/ProjectDetail/ProjectDetail.tsx
@@ -2,21 +2,24 @@ import { Section } from '@tacc/core-components';
 import { useProjects, usePublications, useGrants } from '@tacc/tup-hooks';
 import React from 'react';
 import { useParams } from 'react-router-dom';
-import styles from './ProjectDetails.module.css'
+import styles from './ProjectDetails.module.css';
 
-
-export const ProjectDetails: React.FC<{ projectId: number }> = ({ projectId }) => {
+export const ProjectDetails: React.FC<{ projectId: number }> = ({
+  projectId,
+}) => {
   const details = useProjects();
   const data = details.data ?? [];
-  const projectDetails = data.find((desc) => desc.id === projectId)
-  
+  const projectDetails = data.find((desc) => desc.id === projectId);
+
   const publications = usePublications(projectId);
   const pub = publications.data ?? [];
-  const pub_details = pub.find((publ) => publ.id === projectId)
-  
+  const pub_details = pub.find((publ) => publ.id === projectId);
+
   const grants = useGrants(projectId);
   const grant_data = grants.data ?? [];
-  const grant_details = grant_data.find((grant: { id: number; }) => grant.id === projectId)
+  const grant_details = grant_data.find(
+    (grant: { id: number }) => grant.id === projectId
+  );
 
   return (
     <>
@@ -27,49 +30,48 @@ export const ProjectDetails: React.FC<{ projectId: number }> = ({ projectId }) =
         </span>
       </div>
       <div>
-      <span className={styles['project-detail-header']}>Publications</span>
-         {pub_details === undefined ? 
-         <span className={styles['project-details']}>
-           This project has no publications
-         </span> : 
-         <span className={styles['project-details']}>
-          <span className={styles['project-details-bold']}>
-            {pub_details?.title}
+        <span className={styles['project-detail-header']}>Publications</span>
+        {pub_details === undefined ? (
+          <span className={styles['project-details']}>
+            This project has no publications
           </span>
-          <span>{pub_details?.authors}</span> 
-          <span>
-            <span>{pub_details.publisher}</span>{' '}
-            <span>{pub_details.venue}</span>{' '}
-            <span>{pub_details.yearPublished}</span>
+        ) : (
+          <span className={styles['project-details']}>
+            <span className={styles['project-details-bold']}>
+              {pub_details?.title}
+            </span>
+            <span>{pub_details?.authors}</span>
+            <span>
+              <span>{pub_details.publisher}</span>{' '}
+              <span>{pub_details.venue}</span>{' '}
+              <span>{pub_details.yearPublished}</span>
+            </span>
+            <span>{pub_details?.url}</span>
           </span>
-          <span>{pub_details?.url}</span> 
-        </span>
-         }
+        )}
       </div>
       <div>
-      <span className={styles['project-detail-header']}>Grants</span> 
-        {grant_details === undefined ? 
-        <span className={styles['project-details']}>
-          This project has no grants
-        </span> : 
-        <span>
-          <span className={styles['project-details-bold']}>
-            {grant_details?.title}
+        <span className={styles['project-detail-header']}>Grants</span>
+        {grant_details === undefined ? (
+          <span className={styles['project-details']}>
+            This project has no grants
           </span>
-          <span>Principle Investigator:</span>{' '}
-          {grant_details?.title}
-          <span>{grant_details?.field}</span>
-          <span>Funding Agency:</span>{' '}
-          {grant_details?.fundingAgency}
-          <span>Award Number:</span>{' '}
-          {grant_details?.awardNumber}
-        </span>
-          }
+        ) : (
+          <span>
+            <span className={styles['project-details-bold']}>
+              {grant_details?.title}
+            </span>
+            <span>Principle Investigator:</span> {grant_details?.title}
+            <span>{grant_details?.field}</span>
+            <span>Funding Agency:</span> {grant_details?.fundingAgency}
+            <span>Award Number:</span> {grant_details?.awardNumber}
+          </span>
+        )}
       </div>
     </>
-  )
-}
-    
+  );
+};
+
 const ProjectDetail: React.FC = () => {
   const { projectId } = useParams<{ projectId: string }>();
   if (!projectId) return <div>No project selected.</div>;
@@ -77,11 +79,7 @@ const ProjectDetail: React.FC = () => {
   return (
     <Section
       header={`Project Detail: ${projectId}`}
-      content={
-      <div>
-        {<ProjectDetails projectId={parseInt(projectId)} />}
-          
-      </div>}
+      content={<div>{<ProjectDetails projectId={parseInt(projectId)} />}</div>}
     ></Section>
   );
 };
--- libs/tup-components/src/projects/users/UserList/UserList.tsx
+++ libs/tup-components/src/projects/users/UserList/UserList.tsx
@@ -16,6 +16,7 @@ const UserList: React.FC<{ projectId: number }> = ({ projectId }) => {
         <LoadingSpinner />
       </div>
     );
+  
 
   return (
     <Navbar>
