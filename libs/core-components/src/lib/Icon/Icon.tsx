d3e45caf6680280dc89c7c2b4ccea9d955cb8f7b
Wesley Bomar <wbomar@tacc.utexas.edu>
2023-01-18 15:07:14 -0600

    feat(core-components): Icon, add size prop

--- libs/core-components/src/lib/Icon/Icon.tsx
+++ libs/core-components/src/lib/Icon/Icon.tsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import './Icon.css';
 
+type sizes = 'xs' | 'sm' | 'md' | 'lg';
+
 type IconProps = React.PropsWithChildren<{
   className?: string;
   dataTestid?: string;
   label?: string;
   name: string;
+  size?: sizes;
 }>;
 
-const Icon: React.FC<IconProps> = ({ className, dataTestid, label, name }) => {
-  const iconClassName = `icon icon-${name}`;
+const Icon: React.FC<IconProps> = ({ className, dataTestid, label, name, size }) => {
+  const iconClassName = `icon icon-${name}` + (size
+    ? ` icon-${size}`
+    : '');
   // FAQ: The conditional avoids an extra space in class attribute value
   const fullClassName = className
     ? [className, iconClassName].join(' ')
