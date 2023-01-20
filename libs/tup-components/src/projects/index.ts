0363dc9b78d20045a1795bbacc9659b9145bed44
Sophia Massie <smmassie92@gmail.com>
2023-01-20 12:18:57 -0600

    Format check

--- libs/tup-components/src/projects/index.ts
+++ libs/tup-components/src/projects/index.ts
@@ -4,4 +4,3 @@ export { default as UserList } from './users/UserList/UserList';
 export { default as UserDetail } from './users/UserDetail/UserDetail';
 export { default as ProjectHeader } from './header/ProjectHeader/ProjectHeader';
 export { ProjectsNavbar, ProjectsListing } from './ProjectsListing';
-
