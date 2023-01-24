b40469ba69c901ec4e3f957811c8234c4b057812
Wesley Bomar <wbomar@tacc.utexas.edu>
2023-01-24 09:53:23 -0600

    feat(tup-ui): Dashboard layout without Section
    Adds a global style component `c-page.css` that mimics unused <SectionHeader>.
    
    I think I will just use SectionHeader, in the next commit.

--- apps/tup-ui/src/pages/Dashboard/Dashboard.tsx
+++ apps/tup-ui/src/pages/Dashboard/Dashboard.tsx
@@ -1,5 +1,4 @@
-import React from 'react';
-import { Section } from '@tacc/core-components';
+import React, { useEffect } from 'react';
 import {
   ProfileComponent,
   RequireAuth,
@@ -13,22 +12,31 @@ import './Dashboard.css';
 import styles from './Dashboard.module.css';
 
 const Layout: React.FC = () => {
+  const bodyClassName = 'dashboard';
+
+  useEffect(() => {
+    if (bodyClassName) document.body.classList.add(bodyClassName);
+
+    return function cleanup() {
+      if (bodyClassName) document.body.classList.remove(bodyClassName);
+    };
+  }, [bodyClassName]);
+
   return (
     <RequireAuth>
-      <Section
-        header="Dashboard"
-        bodyClassName="dashboard"
-        contentClassName={styles.panels}
-        content={
-          <>
-            <ProfileComponent />
-            <SystemMonitor />
-            <ProjectsDashboard />
-            <TicketsDashboard />
-            <Outlet />
-          </>
-        }
-      />
+      <section className="c-page">
+        <header>
+          <h1>Dashboard</h1>
+          <a href="https://example.com" target="_blank" rel="noreferrer">Manage Account (needs real link)</a>
+        </header>
+        <main className={styles.panels}>
+          <ProfileComponent />
+          <SystemMonitor />
+          <ProjectsDashboard />
+          <TicketsDashboard />
+          <Outlet />
+        </main>
+      </section>
     </RequireAuth>
   );
 };
