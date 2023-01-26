aa37ecde9a3279a17d4b6e1595fc12b0a9ff6939
Wesley B <62723358+wesleyboar@users.noreply.github.com>
2023-01-26 09:23:00 -0600

    feat(tup-ui): dashboard layout (#90)
    * feat(core-styles/cms): new font sizes
    
    https://github.com/TACC/Core-Styles/pull/90
    
    * fix(tup-ui): login form font sizes
    
    Based on Core-CMS/Styles changes:
    - font size variable value changes
    - body font "12px"
    
    https://confluence.tacc.utexas.edu/x/nh4FDg
    
    * feat(core-styles): polished font settings
    
    Preapre to adapt tup-ui to use:
    - https://github.com/TACC/Core-Styles/pull/90
    - https://github.com/TACC/Core-Styles/pull/92
    - https://github.com/TACC/Core-Styles/pull/93
    
    * feat(core-styles)!: no CMS heading styles
    
    Also no Portal heading styles, but Portal has been like that.
    
    This lets us style fresh in situ, then move to Core-Styles.
    
    * feat(tup-cms): pattern library, better legibility
    
    Loads CMS image that loads core-styles.demo.css in the static/ui/ demo.
    
    * feat(tup-cms): pattern library, better legibility
    
    Loads CMS image that does not force CMS styles on every demo component.
    
    * feat: pattern library font fixes & support changes
    
    * feat: section heading tags & sizes & styles
    
    * feat: dashboard layout
    
    - added Dashboard CSS for Layout
    - added <Section> to Dashboard
    - added tup-ui overrides of CEPv2 spacing from Core-Styles
    - changed <Section> to use CSS grid not flex
    - changed <SectionContent> default layout to `one-column`
    - changed <PageLayout> to prevent `_content` items from overflowing it
    
    * feat(core-cms): new font sizes (#91)
    
    * feat(tup-ui): new font sizes (#88)
    
    * chore(core-comp…): remove fix now in core-styles
    
    * fix(core-comp…): hide "(required)" on login form
    
    * test(tup-ui): fix section tag test case
    
    * fix(tup-cms): misc & cms font size missing 14px
    
    * fix(tup-cms): demo, load missing portal styles
    
    This should fix font sizes in settings/font--portal.
    
    * feat(tup-ui): Dashboard layout without Section
    
    Adds a global style component `c-page.css` that mimics unused <SectionHeader>.
    
    I think I will just use SectionHeader, in the next commit.
    
    * feat(tup-ui): Dashboard header with SectionHeader
    
    Removes global style component `c-page.css` that mimics now used <SectionHeader>.
    
    I had, in previous commit, not used SectionHeader, but why not?
    
    * chore: nx format:write
    
    * Tweak grid css to fix overflow (#110)
    
    Co-authored-by: Jake Rosenberg <jrosenberg@tacc.utexas.edu>

--- apps/tup-ui/src/pages/Dashboard/Dashboard.tsx
+++ apps/tup-ui/src/pages/Dashboard/Dashboard.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { SectionHeader } from '@tacc/core-components';
 import {
   ProfileComponent,
   RequireAuth,
@@ -6,18 +7,40 @@ import {
   ProjectsDashboard,
   TicketsDashboard,
 } from '@tacc/tup-components';
-import { Outlet } from 'react-router-dom';
+
+import './Dashboard.css';
+import styles from './Dashboard.module.css';
 
 const Layout: React.FC = () => {
+  const bodyClassName = 'dashboard';
+
+  useEffect(() => {
+    if (bodyClassName) document.body.classList.add(bodyClassName);
+
+    return function cleanup() {
+      if (bodyClassName) document.body.classList.remove(bodyClassName);
+    };
+  }, [bodyClassName]);
+
   return (
     <RequireAuth>
-      <div>
-        <ProfileComponent />
-        <SystemMonitor />
-        <ProjectsDashboard />
-        <TicketsDashboard />
-        <Outlet />
-      </div>
+      <section className={`c-page ${styles.section}`}>
+        <SectionHeader
+          actions={
+            <a href="https://example.com" target="_blank" rel="noreferrer">
+              Manage Account (needs real link)
+            </a>
+          }
+        >
+          Dashboard
+        </SectionHeader>
+        <main className={styles.panels}>
+          <ProfileComponent />
+          <SystemMonitor />
+          <ProjectsDashboard />
+          <TicketsDashboard />
+        </main>
+      </section>
     </RequireAuth>
   );
 };
