5d8bb154c3e820dffbd692d3f3729ff5135b8f0a
Wesley Bomar <wbomar@tacc.utexas.edu>
2023-01-30 13:10:18 -0600

    feat(tup-cms): user_news as "User Updates"
    Design wants to call these "User Updates", so display it as such (url, text).
    
    Technically, it is news for TACC users i.e. User News. So, continue to name is as such (app name).

--- apps/tup-cms/src/taccsite_cms/urls_custom.py
+++ apps/tup-cms/src/taccsite_cms/urls_custom.py
@@ -5,8 +5,8 @@ from django.conf.urls import url
 custom_urls = [
     path('core/markup/nav/', include('apps.portal_nav.urls', namespace='portal_nav')),
     path('dashboard/', include('apps.dashboard.urls', namespace='dashboard')),
-    path('user_news/', include('apps.user_news.urls', namespace='user_news')),
+    path('user-updates/', include('apps.user_news.urls', namespace='user_news')),
 
-    # Support `taggit_autosuggest` (from `djangocms-blog`)
+    # Supports `taggit_autosuggest` (from `djangocms-blog`)
     url(r'^taggit_autosuggest/', include('taggit_autosuggest.urls')),
 ]
