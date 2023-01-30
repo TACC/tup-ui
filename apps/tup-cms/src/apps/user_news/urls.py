457006d6a4c19d8c8f3fc80bea4cf9d9a0379087
Wesley Bomar <wbomar@tacc.utexas.edu>
2023-01-29 19:09:06 -0600

    feat(tup-cms)!: tup-387, user news remove `list/`
    BREAKING CHANGE: The URL `/user_news/list/` is removed.

--- apps/tup-cms/src/apps/user_news/urls.py
+++ apps/tup-cms/src/apps/user_news/urls.py
@@ -5,6 +5,5 @@ from .views import UserNewsListView, UserNewsReadView
 app_name = 'user_news'
 urlpatterns = [
     path('', UserNewsListView.as_view(), name='index'),
-    path('list/', UserNewsListView.as_view(), name='list'),
     path('<id>/', UserNewsReadView.as_view(), name='read'),
 ]
