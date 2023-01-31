3b64dcc373318fb8a61130c59603dd96d8a39a66
Wesley Bomar <wbomar@tacc.utexas.edu>
2023-01-31 12:29:06 -0600

    feat(user_news): tup-387, plugin structure desc

--- apps/tup-cms/src/apps/user_news/models.py
+++ apps/tup-cms/src/apps/user_news/models.py
@@ -5,6 +5,9 @@ from django.db import models
 from .defaults import max_articles
 
 class UserNewsList(CMSPlugin):
+    def get_short_description(self):
+        return f'{max_articles} articles (max)'
+
     max_articles = models.PositiveSmallIntegerField(
         default=max_articles,
         help_text=f'The maximum number of articles to show.',
