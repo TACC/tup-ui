aaa86fcce7f575c5bdfa7713b45cc140e4aa04cd
jarosenb <jrosenberg@tacc.utexas.edu>
2023-02-02 18:31:28 -0600

    remove jest cruft

--- libs/tup-components/vite.config.ts
+++ libs/tup-components/vite.config.ts
@@ -1,6 +1,6 @@
 /// <reference types="vitest" />
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import react from '@vitejs/plugin-react-swc';
 import viteTsConfigPaths from 'vite-tsconfig-paths';
 import dts from 'vite-plugin-dts';
 import { join } from 'path';
@@ -19,15 +19,6 @@ export default defineConfig({
       root: '../../',
     }),
   ],
-
-  // Uncomment this if you are using workers.
-  // worker: {
-  //  plugins: [
-  //    viteTsConfigPaths({
-  //      root: '../../',
-  //    }),
-  //  ],
-  // },
 
   // Configuration for building your library.
   // See: https://vitejs.dev/guide/build.html#library-mode
@@ -54,6 +45,6 @@ export default defineConfig({
     },
     environment: 'jsdom',
     include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
-    setupFiles: ['./jest.setup.ts'],
+    setupFiles: ['./vitest.setup.ts'],
   },
 });
--- libs/tup-hooks/vite.config.ts
+++ libs/tup-hooks/vite.config.ts
@@ -54,6 +54,6 @@ export default defineConfig({
     },
     environment: 'jsdom',
     include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
-    setupFiles: ['./jest.setup.ts'],
+    setupFiles: ['./vitest.setup.ts'],
   },
 });
