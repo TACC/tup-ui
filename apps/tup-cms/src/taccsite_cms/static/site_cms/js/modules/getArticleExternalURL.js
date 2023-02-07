e17d7e51db6c779ef93f146885228ece8b4b5487
Wesley Bomar <wbomar@tacc.utexas.edu>
2023-02-07 09:23:40 -0600

    chore(tup-cms): nx format:write

--- apps/tup-cms/src/taccsite_cms/static/site_cms/js/modules/getArticleExternalURL.js
+++ apps/tup-cms/src/taccsite_cms/static/site_cms/js/modules/getArticleExternalURL.js
@@ -3,16 +3,16 @@
  * @param {HTMLElement} article - The article within which to find external URL
  * @return {string|null} - The URL of the external article
  */
-export default function getArticleExternalURL( article ) {
-  if ( ! article ) {
-    console.warn('No article found', article );
+export default function getArticleExternalURL(article) {
+  if (!article) {
+    console.warn('No article found', article);
     return;
   }
 
   // News admin states only one external link should be in article abstract
   const contentLink = article.querySelector('.blog-lead a[href^="http"]');
-  const isExternalLink = ( contentLink.hostname !== window.location.hostname );
-  const URL = ( isExternalLink ) ? contentLink.href : null;
+  const isExternalLink = contentLink.hostname !== window.location.hostname;
+  const URL = isExternalLink ? contentLink.href : null;
 
   return URL;
 }
