1547d62280d6bf72f3380800ce15530191e8c360
Van Go <35277477+van-go@users.noreply.github.com>
2023-02-10 08:07:33 -0600

    tup-408-mfa-help-link

--- libs/tup-components/src/mfa/MfaHeader.tsx
+++ libs/tup-components/src/mfa/MfaHeader.tsx
@@ -1,12 +1,13 @@
 import { Link } from 'react-router-dom';
 import styles from './Mfa.module.css';
+import TicketCreateModal from '../tickets/TicketCreateModal';
 
 const MfaHeader: React.FC = () => {
   return (
     <div className={styles['mfa-header']}>
       <div>Multifactor Authentication Pairing</div>
       <div>
-        Get Help | <Link to="/account">Exit Pairing Process</Link>
+      <TicketCreateModal /> | <Link to="/account">Exit Pairing Process</Link>
       </div>
     </div>
   );
