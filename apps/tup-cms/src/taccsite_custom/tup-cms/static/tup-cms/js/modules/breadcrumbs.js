bcde9cae1be4c580a05ce1012fe3ae3b597abe41
Wesley Bomar <wbomar@tacc.utexas.edu>
2023-03-28 13:10:35 -0500

    chore(tup-cms): nx format:write

--- apps/tup-cms/src/taccsite_custom/tup-cms/static/tup-cms/js/modules/breadcrumbs.js
+++ apps/tup-cms/src/taccsite_custom/tup-cms/static/tup-cms/js/modules/breadcrumbs.js
@@ -4,9 +4,11 @@
  * To "disable" top-level CMS menu nav links in breadcrumbs
  * (because they all are always set to redirect to a child)
  */
-const link = document.querySelector('.s-breadcrumbs:is(nav) li:nth-of-type(2) a');
-const isAppropriatePage = (window.location.pathname.search('/systems/') == -1);
+const link = document.querySelector(
+  '.s-breadcrumbs:is(nav) li:nth-of-type(2) a'
+);
+const isAppropriatePage = window.location.pathname.search('/systems/') == -1;
 
-if ( isAppropriatePage ) {
-    link.removeAttribute('href');
+if (isAppropriatePage) {
+  link.removeAttribute('href');
 }
