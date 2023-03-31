31d7e8cc997b1ed9e6aca02d97a0983eff6b4477
Wesley B <62723358+wesleyboar@users.noreply.github.com>
2023-03-31 16:42:27 -0500

    fix(tup-cms): full width template, avoid js error (#206)
    On a full width template, no breadcrumbs exist, thus no `link` exists from which to `removeAttribute`.

--- apps/tup-cms/src/taccsite_custom/tup-cms/static/tup-cms/js/modules/breadcrumbs.js
+++ apps/tup-cms/src/taccsite_custom/tup-cms/static/tup-cms/js/modules/breadcrumbs.js
@@ -1,5 +1,8 @@
-// TODO: Move to TACC/Core-CMS
-// TODO: Try to programmatically only perform if link is a redirect
+/* TODO: Try to programmatically only perform if link is a redirect */
+/* TODO: Integrate into Core-CMS (not Core-Styles):
+        - …/css/src/imports/trumps/s-breadcrumbs.css
+        - …/js/modules/breadcrumbs.js
+*/
 /**
  * To "disable" top-level CMS menu nav links in breadcrumbs
  * (because they all are always set to redirect to a child)
@@ -9,6 +12,6 @@ const link = document.querySelector(
 );
 const isAppropriatePage = window.location.pathname.search('/systems/') == -1;
 
-if (isAppropriatePage) {
+if (link && isAppropriatePage) {
   link.removeAttribute('href');
 }
