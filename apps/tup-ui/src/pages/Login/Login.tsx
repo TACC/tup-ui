2eafd20aaee00f2c7513b3d715bf32f05fecf75c
Wesley Bomar <wbomar@tacc.utexas.edu>
2023-04-03 12:55:34 -0500

    fix(tup-ui): body gray bkgd only for login page

--- apps/tup-ui/src/pages/Login/Login.tsx
+++ apps/tup-ui/src/pages/Login/Login.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { LoginComponent } from '@tacc/tup-components';
 import { Button } from '@tacc/core-components';
 
@@ -6,6 +6,16 @@ import './Login.global.css';
 import styles from './Login.module.css';
 
 const Layout: React.FC = () => {
+  const bodyClassName = 'login';
+
+  useEffect(() => {
+    if (bodyClassName) document.body.classList.add(bodyClassName);
+
+    return function cleanup() {
+      if (bodyClassName) document.body.classList.remove(bodyClassName);
+    };
+  }, [bodyClassName]);
+
   return (
     <div className={styles.root}>
       <LoginComponent className={styles.body} />
