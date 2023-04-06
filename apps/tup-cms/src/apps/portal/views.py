7d6a79ca80100c65f43d0074a8c70e3b7a3de202
Wesley Bomar <wbomar@tacc.utexas.edu>
2023-04-06 15:01:39 -0500

    chore(tup-cms): remove extra whitespace

--- apps/tup-cms/src/apps/portal/views.py
+++ apps/tup-cms/src/apps/portal/views.py
@@ -36,17 +36,17 @@ def ImpersonateView(request):
     resp = HttpResponseRedirect("/portal/dashboard")
     if not request.user.is_staff:
         return resp
-    
+
     headers = {"x-tup-token": settings.TUP_SERVICES_ADMIN_JWT}
     data = {"username": request.GET.get("username")}
-    
+
     impersonation_resp = requests.post(f"{service_url}/auth/impersonate",
                                        headers=headers,
                                        json=data)
     user_jwt = impersonation_resp.json()['jwt']
     resp.set_cookie("x-tup-token", user_jwt)
     return resp
-    
+
 
 @tup_login_required()
 def PortalView(request):
