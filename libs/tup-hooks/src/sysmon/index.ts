663181e4308a352fccac917c2515b14d1701ac50
Sophia Massie <smmassie92@gmail.com>
2023-04-27 11:29:41 -0500

    Add hooks for system queue response

--- libs/tup-hooks/src/sysmon/index.ts
+++ libs/tup-hooks/src/sysmon/index.ts
@@ -22,4 +22,23 @@ export type SystemMonitorRawSystem = {
   reservations?: [Reservation];
 };
 
-export { default as useSystemMonitor } from './useSystemMonitor';
+export type SystemQueue = {
+  display_name: string;
+  tas_name: string;
+  hostname: string;
+  system_type: string;
+  timestamp?: string;
+  queues?: {[queueName: string]: JobsQueue};
+};
+
+export type JobsQueue = {
+  down?: boolean;
+  hidden?: boolean;
+  load?: number;
+  free?: number;
+  running?: number;
+  waiting?: number;
+
+};
+
+export { useSystemMonitor, useSystemQueue } from './useSystemMonitor';
