f1533d83ccdc2b758d75a5be717e538ab7074680
Jake Rosenberg <jrosenberg@MacBook-Pro.lan>
2023-06-08 15:51:33 -0500

    Prevent users from getting locked into SMS pairing

--- libs/tup-components/src/mfa/MfaSelection.tsx
+++ libs/tup-components/src/mfa/MfaSelection.tsx
@@ -5,16 +5,6 @@ import { Link, Navigate } from 'react-router-dom';
 import { useMfa } from '@tacc/tup-hooks';
 
 const MfaSelector: React.FC = () => {
-  const { data } = useMfa();
-
-  // If users have already generated a code with one method,
-  // they will need to complete registration with that method.
-  if (data?.token && data?.token.tokentype === 'sms') {
-    return <Navigate to="/mfa/sms" replace={true} />;
-  }
-  if (data?.token && data?.token.tokentype === 'totp') {
-    return <Navigate to="/mfa/totp" replace={true} />;
-  }
 
   return (
     <div className={styles['mfa-type-container']}>
