ab09e1843f1bcc12f77deb82b44bc192ed89ea9a
Sophia Massie <smmassie92@gmail.com>
2023-07-07 11:51:35 -0500

    Changed from system_monitor to system-status and updated tests

---
+++ libs/tup-components/src/system-status/SystemMonitorCells.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Pill } from '@tacc/core-components';
+import { Cell } from 'react-table';
+import { Link } from 'react-router-dom';
+import { SystemMonitorRawSystem } from '@tacc/tup-hooks';
+
+export const Display: React.FC<{
+  cell: Cell<SystemMonitorRawSystem, string>;
+}> = ({ cell: { row } }) => (
+  <Link to={`/system-status/${row.original.tas_name}`}>
+    {row.original.display_name}
+  </Link>
+);
+
+export const Operational: React.FC<{
+  cell: Cell<SystemMonitorRawSystem, boolean>;
+}> = ({ cell: { value } }) => {
+  if (value) {
+    return <Pill type="normal">Operational</Pill>;
+  }
+  return <Pill type="warning">Maintenance</Pill>;
+};
+
+export const Load: React.FC<{
+  cell: Cell<SystemMonitorRawSystem, number | undefined>;
+}> = ({ cell: { value } }) => <span>{value ? `${value}%` : '--'}</span>;
---
+++ libs/tup-components/src/system-status/index.ts
@@ -0,0 +1,2 @@
+export { SystemMonitor, SystemMonitorTable } from './SystemMonitor';
+export { SystemNavBar, SystemDetails } from './details';
