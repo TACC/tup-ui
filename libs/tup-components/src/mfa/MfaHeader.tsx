e88f365c5cc1a834785aa25da8a5b8e4769aadfa
Wesley Bomar <wbomar@tacc.utexas.edu>
2023-08-02 14:01:38 -0500

    chore: nx format:write

--- libs/tup-components/src/mfa/MfaHeader.tsx
+++ libs/tup-components/src/mfa/MfaHeader.tsx
@@ -3,16 +3,16 @@ import TicketCreateModal from '../tickets/TicketCreateModal';
 
 const MfaHeader: React.FC = () => {
   const Actions = (
-      <nav className="c-nav c-nav--no-list c-nav--piped">
-        <TicketCreateModal display="link">Get Help</TicketCreateModal>
-        <a
-          target="_blank"
-          rel="noreferrer"
-          href="https://docs.tacc.utexas.edu/basics/mfa/"
-        >
-          MFA Documentation
-        </a>
-      </nav>
+    <nav className="c-nav c-nav--no-list c-nav--piped">
+      <TicketCreateModal display="link">Get Help</TicketCreateModal>
+      <a
+        target="_blank"
+        rel="noreferrer"
+        href="https://docs.tacc.utexas.edu/basics/mfa/"
+      >
+        MFA Documentation
+      </a>
+    </nav>
   );
 
   return (
