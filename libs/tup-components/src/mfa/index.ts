60afcbb4db3b29d9049b7c0fe328b9a105b840a8
Wesley Bomar <wbomar@tacc.utexas.edu>
2023-08-09 18:07:35 -0500

    test: restore MfaWrapper (incomplete)

--- libs/tup-components/src/mfa/index.ts
+++ libs/tup-components/src/mfa/index.ts
@@ -2,3 +2,4 @@ export { default as MfaSelection } from './MfaSelection';
 export { default as MfaHeader } from './MfaHeader';
 export { default as MfaPairingview } from './MfaPairingView';
 export { default as MfaUnpairingview } from './MfaUnpairingView';
+export { default as MfaWrapper } from './MfaWrapper';
