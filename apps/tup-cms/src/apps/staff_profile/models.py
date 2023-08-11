83c2aaa499f6da3b82f8c2d9c90b61a3b249e9a6
Wesley Bomar <wbomar@tacc.utexas.edu>
2023-08-11 10:50:11 -0500

    fix: only require names, title, and email
    i.e. let all other fields `blank=True`

--- apps/tup-cms/src/apps/staff_profile/models.py
+++ apps/tup-cms/src/apps/staff_profile/models.py
@@ -8,20 +8,20 @@ class StaffProfilePlugin(CMSPlugin):
     """
     Model for CMS staff profile pages.
     """
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
-    title = models.CharField(max_length=100)
-    email = models.CharField(max_length=100)
-    department = models.CharField(max_length=100)
-    phone = models.CharField(max_length=50, null=True)
+    first_name = models.CharField(max_length=100, blank=False)
+    last_name = models.CharField(max_length=100, blank=False)
+    title = models.CharField(max_length=100, blank=False)
+    email = models.CharField(max_length=100, blank=False)
+    department = models.CharField(max_length=100, blank=True)
+    phone = models.CharField(max_length=50, null=True, blank=True)
 
     description = models.TextField()
 
-    photo = FilerImageField(on_delete=models.CASCADE, null=True)
+    photo = FilerImageField(on_delete=models.CASCADE, null=True, blank=True)
 
-    publications = models.TextField(null=True)
-    projects = models.TextField(null=True)
-    education = models.TextField(null=True)
-    research_areas = models.TextField(null=True)
-    memberships = models.TextField(null=True)
-    experience = models.TextField(null=True)
+    publications = models.TextField(null=True, blank=True)
+    projects = models.TextField(null=True, blank=True)
+    education = models.TextField(null=True, blank=True)
+    research_areas = models.TextField(null=True, blank=True)
+    memberships = models.TextField(null=True, blank=True)
+    experience = models.TextField(null=True, blank=True)
