d992a73c55cd1a17a967bd2320e49ad9f3e5b57a
Wesley Bomar <wbomar@tacc.utexas.edu>
2023-08-11 15:18:09 -0500

    refactor: move all ui context work into template

--- apps/tup-cms/src/apps/staff_profile/cms_plugins.py
+++ apps/tup-cms/src/apps/staff_profile/cms_plugins.py
@@ -2,7 +2,6 @@ from cms.plugin_base import CMSPluginBase
 from cms.plugin_pool import plugin_pool
 from cms.models.pluginmodel import CMSPlugin
 from django.utils.translation import gettext_lazy as _
-from django.utils.html import escape
 from djangocms_text_ckeditor.cms_plugins import TextPlugin
 from django.forms.models import ModelForm
 from django.db import models
@@ -11,14 +10,6 @@ from djangocms_text_ckeditor.widgets import TextEditorWidget
 
 from .models import StaffProfilePlugin
 from .forms import StaffProfilePluginForm
-
-def get_display_name(instance):
-    name = f'{instance.first_name} {instance.first_name}'
-
-    if instance.post_nomial:
-        return f'{name}, {instance.post_nomial}'
-    else:
-        return name
 
 @plugin_pool.register_plugin
 class StaffProfilePlugin(CMSPluginBase):
@@ -32,10 +23,5 @@ class StaffProfilePlugin(CMSPluginBase):
 
     def render(self, context, instance, placeholder):
         context = super().render(context, instance, placeholder)
-
-        context.update({
-            'display_name': escape(get_display_name(instance)),
-            'fallback_alt_text': escape(get_display_name(instance))
-        })
 
         return context
