9c6fa8ffa5e6fc66af2f6f7c99eb7be9bee90930
Wesley Bomar <wbomar@tacc.utexas.edu>
2023-08-16 17:39:45 -0500

    fix: bugs in staff_profile js — ⚠️ incomplete
    Warning: Image is only moved once. Image is not found after move. Error.

--- apps/tup-cms/src/apps/staff_profile/static/staff_profile/js/staff_profile.js
+++ apps/tup-cms/src/apps/staff_profile/static/staff_profile/js/staff_profile.js
@@ -2,23 +2,30 @@
 /* https://www.tacc.utexas.edu/about/staff-directory/.../ */
 /* FAQ: Not specific to plugin, because manual staff profiles use this too */
 
+const col1 = document.querySelector('.row.c-staff-profile > .col:nth-child(1)');
+const col2 = document.querySelector('.row.c-staff-profile > .col:nth-child(2)');
+const image = col2.querySelector(':scope > *:first-child');
+const header = col1.querySelector(':scope > *:first-child');
+
 function moveImage() {
   const isWideScreen = window.matchMedia('(min-width: 768px)');
-  const isImageInWideSpot = col2.contains( img );
+  const isImageInWideSpot = col2.contains( image );
+  const shouldMoveImage = ( isWideScreen.matches && ! isImageInWideSpot );
 
-  if ( isWideScreen && ! isImageInWideSpot ) {
+  console.log({
+    isImageInWideSpot,
+    isWideScreen: isWideScreen.matches,
+    image,
+    shouldMoveImage,
+    imagePreviousSibling: image.previousSibling.outerHTML
+  })
+
+  if ( shouldMoveImage ) {
     col2.prepend( image );
   } else {
-    image.insertAfter( header );
+    header.after( image );
   }
 }
 
-export function rearrange() {
-  const col1 = document.querySelector('.row.c-staff-profile > .col:nth-child(1)');
-  const col2 = document.querySelector('.row.c-staff-profile > .col:nth-child(2)');
-  const image = col2.querySelector(':scope > *:first-child');
-  const header = col1.querySelector(':scope > *:first-child');
-
-  moveImage();
-  window.on('resize', () => moveImage() );
-}
+moveImage();
+window.addEventListener('resize', () => moveImage() );
