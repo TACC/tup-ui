00ea82dc2722fe37d635871df63757a5cf691968
Wesley Bomar <wbomar@tacc.utexas.edu>
2023-08-17 14:19:50 -0500

    chore: remove excess property on admin class

--- apps/tup-cms/src/apps/staff_profile/admin.py
+++ apps/tup-cms/src/apps/staff_profile/admin.py
@@ -9,6 +9,5 @@ class StaffProfilePluginAdmin(FrontendEditableAdminMixin, admin.ModelAdmin):
     list_display = ('id', 'first_name', 'last_name', 'department', 'email')
     list_filter = ('department', 'title')
     search_fields = ('first_name', 'last_name', 'email')
-    frontend_editable_fields = ('first_name', 'last_name', 'post_nomials', 'title', 'title2', 'email', 'department', 'phone', 'description', 'photo', 'publications', 'projects', 'education', 'research_areas', 'memberships')
 
 admin.site.register(StaffProfilePlugin, StaffProfilePluginAdmin)
