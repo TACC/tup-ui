a3819d6ae425486fd82a30818ecb2c1a7d0501ab
Wesley Bomar <wbomar@tacc.utexas.edu>
2023-08-24 15:30:14 -0500

    chore!: manaually merge all migrations into 1

--- apps/tup-cms/src/apps/staff_profile/migrations/0001_squashed_for_release.py
+++ apps/tup-cms/src/apps/staff_profile/migrations/0001_squashed_for_release.py
@@ -8,7 +8,16 @@ import filer.fields.image
 
 class Migration(migrations.Migration):
 
-    replaces = [('staff_profile', '0001_initial'), ('staff_profile', '0002_alter_staffprofileplugin_phone')]
+    replaces = [
+      ('staff_profile', '0001_initial'),
+      ('staff_profile', '0002_alter_staffprofileplugin_phone'),
+      ('staff_profile', '0001_squashed_0002_alter_staffprofileplugin_phone'),
+      ('staff_profile', '0002_split_name_into_first_and_last'),
+      ('staff_profile', '0003_reduce_first_last_name_length_squashed_0009_do_not_require_photo'),
+      ('staff_profile', '0004_reduce_name_field_lengths'),
+      ('staff_profile', '0005_remove_experience'),
+      ('staff_profile', '0006_reduce_post_nomials_length'),
+    ]
 
     initial = True
 
@@ -22,8 +31,11 @@ class Migration(migrations.Migration):
             name='StaffProfilePlugin',
             fields=[
                 ('cmsplugin_ptr', models.OneToOneField(auto_created=True, on_delete=django.db.models.deletion.CASCADE, parent_link=True, primary_key=True, related_name='staff_profile_staffprofileplugin', serialize=False, to='cms.cmsplugin')),
-                ('name', models.CharField(max_length=200)),
+                ('first_name', models.CharField(max_length=100)),
+                ('last_name', models.CharField(max_length=100)),
+                ('post_nomials', models.CharField(max_length=50)),
                 ('title', models.CharField(max_length=100)),
+                ('title2', models.CharField(max_length=100)),
                 ('email', models.CharField(max_length=100)),
                 ('department', models.CharField(max_length=100)),
                 ('phone', models.CharField(max_length=50, null=True)),
@@ -33,8 +45,7 @@ class Migration(migrations.Migration):
                 ('education', models.TextField(null=True)),
                 ('research_areas', models.TextField(null=True)),
                 ('memberships', models.TextField(null=True)),
-                ('experience', models.TextField(null=True)),
-                ('photo', filer.fields.image.FilerImageField(null=True, on_delete=django.db.models.deletion.CASCADE, to=settings.FILER_IMAGE_MODEL)),
+                ('photo', filer.fields.image.FilerImageField(blank=True, null=True, on_delete=django.db.models.deletion.CASCADE, to=settings.FILER_IMAGE_MODEL)),
             ],
             options={
                 'abstract': False,
