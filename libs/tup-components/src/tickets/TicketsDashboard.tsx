790a6b830c76d43a575200bb670b33c8a1281f79
Wesley Bomar <wbomar@tacc.utexas.edu>
2023-09-07 17:21:46 -0500

    feat: add link in Dashboard to "View all tickets"

--- libs/tup-components/src/tickets/TicketsDashboard.tsx
+++ libs/tup-components/src/tickets/TicketsDashboard.tsx
@@ -1,14 +1,22 @@
-import { SectionTableWrapper } from '@tacc/core-components';
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { SectionTableWrapper } from '@tacc/core-components';
 import TicketCreateModal from './TicketCreateModal';
 import { TicketsTable } from './TicketsTable';
+
+import styles from './TicketsDashboard.module.css';
 
 const TicketsDashboard: React.FC = () => {
   return (
     <SectionTableWrapper
       header="My Tickets"
       headerActions={
-        <TicketCreateModal display="secondary">+ New Ticket</TicketCreateModal>
+        <>
+          <Link to="/tickets" className={styles['viewall-action']}>
+            View all tickets
+          </Link>
+          <TicketCreateModal display="secondary">+ New Ticket</TicketCreateModal>
+        </>
       }
       contentShouldScroll
     >
