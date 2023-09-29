6da7cd2140274932eb3c6c300fa46b1e78c70fc8
Jake Rosenberg <jrosenberg@MacBook-Pro.lan>
2023-09-29 11:41:53 -0500

    replace Manage Team modal with link to TAM invitation page

--- libs/tup-components/src/projects/users/UserList/ManageTeam.tsx
+++ libs/tup-components/src/projects/users/UserList/ManageTeam.tsx
@@ -1,7 +1,6 @@
 import { Button } from '@tacc/core-components';
 import { useProject, useRoleForCurrentUser } from '@tacc/tup-hooks';
 import styles from './UserList.module.css';
-import AddUserModal from './AddUserModal';
 
 const ManageTeam: React.FC<{ projectId: number }> = ({ projectId }) => {
   const currentUserRole = useRoleForCurrentUser(projectId) ?? '';
@@ -26,7 +25,13 @@ const ManageTeam: React.FC<{ projectId: number }> = ({ projectId }) => {
 
   return (
     <div className={styles['user-navactions']}>
-      <AddUserModal projectId={projectId} />
+      <a
+        target="_blank"
+        rel="noreferrer"
+        href={`https://accounts.tacc.utexas.edu/project_invite?project_id=${projectId}`}
+      >
+        <Button>+ Add Users</Button>
+      </a>
     </div>
   );
 };
