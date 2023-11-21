cc7763b81df0e49cb2f0c155bb2a6a09709fdf9b
Wesley B <62723358+wesleyboar@users.noreply.github.com>
2023-11-20 18:07:22 -0600

    chore: tup-463 delete css modules mimicking core (#377)
    Some Core-Styles CSS was added after TUP login page was developed.
    
    Now that it is available, TUP login page can use it. Less module CSS!

--- apps/tup-ui/src/pages/Login/Login.tsx
+++ apps/tup-ui/src/pages/Login/Login.tsx
@@ -1,23 +1,14 @@
 import React from 'react';
 import { LoginComponent } from '@tacc/tup-components';
 
-import styles from './Login.module.css';
-
 const Layout: React.FC = () => {
   return (
-    <div className={styles.root}>
-      <LoginComponent className={styles.body} />
-      <div className={styles.footer}>
-        <a
-          className={styles.footer__link}
-          href="/about/security-and-compliance/"
-        >
-          Security
-        </a>
-        <a className={styles.footer__link} href="/use-tacc/user-policies/">
-          Policies
-        </a>
-      </div>
+    <div className="s-form-page">
+      <LoginComponent />
+      <footer>
+        <a href="/about/security-and-compliance/">Security</a>
+        <a href="/use-tacc/user-policies/">Policies</a>
+      </footer>
     </div>
   );
 };
