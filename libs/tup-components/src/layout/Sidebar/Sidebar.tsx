34c712be7d34d6cfd9d694b9e0cda5e81ce4a200
Tomas Gonzalez <tgonzalez@tacc.utexas.edu>
2024-01-31 15:36:31 -0600

    adds text under icons back

--- libs/tup-components/src/layout/Sidebar/Sidebar.tsx
+++ libs/tup-components/src/layout/Sidebar/Sidebar.tsx
@@ -9,29 +9,19 @@ const Sidebar: React.FC = () => {
     <div className={styles['root']}>
       <Navbar>
         <NavItem end icon="dashboard" to={'/dashboard'}>
-          <span className='d-none d-md-inline'>
             Dashboard
-          </span>
         </NavItem>
         <NavItem icon="allocations" to={'/projects'}>
-          <span className="d-none d-md-inline">
             Projects & Allocations
-          </span>
         </NavItem>
         <NavItem icon="multiple-coversation" to={'/tickets'}>
-          <span className="d-none d-md-inline">
             Tickets
-          </span>
         </NavItem>
         <NavItem icon="data-files" to={'/system-status'}>
-          <span className="d-none d-md-inline">
             System Status
-          </span>
         </NavItem>
         <NavItem icon="user" to={'/account'}>
-          <span className="d-none d-md-inline">
             Manage Account
-          </span>
         </NavItem>
       </Navbar>
     </div>
