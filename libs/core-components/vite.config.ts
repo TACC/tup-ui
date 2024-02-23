f9c59448d114f4d62da00d066f15c828dc9d4fba
Wesley Bomar <wbomar@tacc.utexas.edu>
2024-02-23 17:27:20 -0600

    feat: tup-700 1st attempt to split files/css build

--- libs/core-components/vite.config.ts
+++ libs/core-components/vite.config.ts
@@ -1,10 +1,26 @@
 /// <reference types="vitest" />
-import { resolve } from 'path';
+import { extname, relative, resolve } from 'path';
+import { fileURLToPath } from 'node:url';
+import { glob } from 'glob';
+
 import { defineConfig } from 'vite';
 // import dts from 'vite-plugin-dts';
 import react from '@vitejs/plugin-react-swc';
 import { libInjectCss } from 'vite-plugin-lib-inject-css'
 import viteTsConfigPaths from 'vite-tsconfig-paths';
+
+const globOutput = glob.sync(resolve(__dirname, 'src/lib/**/*.{ts,tsx,js,jsx}')).map(file => [
+  // This removes `...src/` as well as the file extension from each
+  // file, so e.g. ...src/nested/foo.js becomes nested/foo
+  relative(
+    resolve(__dirname, 'src/lib'),
+    file.slice(0, file.length - extname(file).length)
+  ),
+  // This expands the relative paths to absolute paths, so e.g.
+  // ...src/nested/foo becomes /project/src/nested/foo.js
+  fileURLToPath(new URL(file, import.meta.url))
+]);
+// console.log( globOutput );
 
 export default defineConfig({
   cacheDir: '../../node_modules/.vite/core-components',
@@ -32,11 +48,14 @@ export default defineConfig({
     },
     rollupOptions: {
       external: ['react', 'react/jsx-runtime'],
-      // output: {
-      //   globals: {
-      //     vue: 'React',
-      //   },
-      // },
+      input: Object.fromEntries(
+        // https://rollupjs.org/configuration-options/#input
+        globOutput
+      ),
+      output: {
+        assetFileNames: 'assets/[name][extname]',
+        entryFileNames: '[name].js',
+      }
     },
   },
 
