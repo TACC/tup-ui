ba5f2950a033c5f9a3f374d9618a63f47e3e0efa
Wesley Bomar <wbomar@tacc.utexas.edu>
2024-02-27 13:23:09 -0600

    feat: tup-700 button story

---
+++ libs/core-components/src/lib/Button/Button.stories.tsx
@@ -0,0 +1,32 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import Button, { SIZE_MAP } from './Button';
+
+import styles from './Button.stories.module.css';
+
+const meta: Meta<typeof Button> = {
+  component: Button,
+  argTypes: {
+    size: {
+      options: Object.keys(SIZE_MAP),
+      control: { type: 'inline-radio' },
+    },
+  },
+};
+export default meta;
+
+type Story = StoryObj<typeof Button>;
+
+export const Types: Story = {
+  render: (args) => (
+    <>
+      <Button type="primary" {...args}>Primary</Button>
+      <Button type="secondary" {...args}>Secondary</Button>
+      <Button type="tertiary" {...args}>Tertiary</Button>
+      <Button type="link" {...args}>as Link</Button>
+    </>
+  ),
+  args: {
+    className: styles['button'] + ' '
+  },
+};
