51e58ab12603b6c7fff956aaf2bd225734f41fe5
Wesley Bomar <wbomar@tacc.utexas.edu>
2024-05-09 18:34:54 -0500

    npx nx format:write

--- libs/core-wrappers/src/index.ts
+++ libs/core-wrappers/src/index.ts
@@ -1,5 +1,10 @@
 import { WizardStep as WizardStepType } from './lib/Wizard';
-export { Navbar, NavItem, QueryNavItem, AnchorNavItem } from './lib/NavbarWrapper';
+export {
+  Navbar,
+  NavItem,
+  QueryNavItem,
+  AnchorNavItem,
+} from './lib/NavbarWrapper';
 export { default as QueryWrapper } from './lib/QueryWrapper';
 export { default as SubmitWrapper } from './lib/SubmitWrapper';
 export { default as Wizard, useWizard, WizardNavigation } from './lib/Wizard';
