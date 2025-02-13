f728de26e621fa67ad5c985169245b23408d4397
Jake Rosenberg <jrosenberg@tacc.utexas.edu>
2025-02-12 18:25:21 -0600

    service url fix

--- apps/tup-cms/src/apps/portal/apps.py
+++ apps/tup-cms/src/apps/portal/apps.py
@@ -8,7 +8,7 @@ from django.core.mail import send_mail
 from .utils import reverse_slugify
 
 logger = logging.getLogger(f"portal.{__name__}")
-service_url = settings.TUP_SERVICES_URL
+service_url = "http://nginx:80/tup-api"
 
 QUEUE_MAP = {
     "Allocations": "Allocations",
