1d34cd34e4b5acfb840c097270b5536acb4ad76c
Sophia Massie-Perez <smassie@smassie-mbp25.local>
2025-10-31 12:52:14 -0500

    Linting

--- apps/tup-ui/src/pages/Projects/Projects.tsx
+++ apps/tup-ui/src/pages/Projects/Projects.tsx
@@ -22,18 +22,31 @@ const Layout: React.FC = () => {
       <section className={styles['project-section']}>
         <PageLayout
           top={
-          <>
-            <SectionHeader actions={<NewProject />} isNestedHeader>
-              Projects & Allocations
-            </SectionHeader>
-            <SectionHeader>
-            {import.meta.env.VITE_FEATURE_PROJECTS_ENABLED &&
-              <SectionMessage type="error" className={styles['banner-message']}>
-                Project Maintenance is <time>Tues Nov 4, 9AM - 1PM CT</time>. You will be unable to manage your project users during this time. See <a href="https://tacc.utexas.edu/news/user-updates/107601/" rel="noreferrer" target="_blank">tacc.utexas.edu/news/user-updates/107601</a>.
-              </SectionMessage>
-            }
-            </SectionHeader>
-          </>
+            <>
+              <SectionHeader actions={<NewProject />} isNestedHeader>
+                Projects & Allocations
+              </SectionHeader>
+              <SectionHeader>
+                {import.meta.env.VITE_FEATURE_PROJECTS_ENABLED && (
+                  <SectionMessage
+                    type="error"
+                    className={styles['banner-message']}
+                  >
+                    Project Maintenance is <time>Tues Nov 4, 9AM - 1PM CT</time>
+                    . You will be unable to manage your project users during
+                    this time. See{' '}
+                    <a
+                      href="https://tacc.utexas.edu/news/user-updates/107601/"
+                      rel="noreferrer"
+                      target="_blank"
+                    >
+                      tacc.utexas.edu/news/user-updates/107601
+                    </a>
+                    .
+                  </SectionMessage>
+                )}
+              </SectionHeader>
+            </>
           }
           left={<ProjectsNavbar />}
           right={<ProjectsListing />}
