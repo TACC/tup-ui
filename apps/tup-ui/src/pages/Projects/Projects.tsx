ebffcc1fde443a8e4195ab6c042714503047529b
Sophia Massie-Perez <smassie@tacc.utexas.edu>
2025-10-31 13:46:51 -0500

    Adjust font size

--- apps/tup-ui/src/pages/Projects/Projects.tsx
+++ apps/tup-ui/src/pages/Projects/Projects.tsx
@@ -26,15 +26,15 @@ const Layout: React.FC = () => {
               <SectionHeader actions={<NewProject />} isNestedHeader>
                 Projects & Allocations
               </SectionHeader>
-              <SectionHeader>
-                {import.meta.env.VITE_FEATURE_PROJECTS_ENABLED && (
+              {import.meta.env.VITE_FEATURE_PROJECTS_ENABLED && (
+                <>
                   <SectionMessage
                     type="error"
                     className={styles['banner-message']}
                   >
-                    Project Maintenance is <time>Tues Nov 4, 9AM - 1PM CT</time>
-                    . You will be unable to manage your project users during
-                    this time. See{' '}
+                    Project Maintenance is scheduled for{' '}
+                    <time>Tues Nov 4, 9AM - 1PM CT</time>. You will be unable to
+                    manage your project users during this time. See{' '}
                     <a
                       href="https://tacc.utexas.edu/news/user-updates/107601/"
                       rel="noreferrer"
@@ -44,8 +44,9 @@ const Layout: React.FC = () => {
                     </a>
                     .
                   </SectionMessage>
-                )}
-              </SectionHeader>
+                  <hr />
+                </>
+              )}
             </>
           }
           left={<ProjectsNavbar />}
