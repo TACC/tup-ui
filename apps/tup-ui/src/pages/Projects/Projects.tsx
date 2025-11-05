a647df9559605680d8c4235567d82944fdd5f395
Wesley B <62723358+wesleyboar@users.noreply.github.com>
2025-11-05 16:32:28 -0600

    fix: remove message

--- apps/tup-ui/src/pages/Projects/Projects.tsx
+++ apps/tup-ui/src/pages/Projects/Projects.tsx
@@ -22,29 +22,9 @@ const Layout: React.FC = () => {
       <section className={styles['project-section']}>
         <PageLayout
           top={
-            <>
-              {import.meta.env.VITE_FEATURE_PROJECTS_ENABLED !== 'true' && (
-                <SectionMessage
-                  type="error"
-                  className={styles['banner-message']}
-                >
-                  Project Maintenance is scheduled for{' '}
-                  <time>Tues Nov 4, 9AM - 1PM CT</time>. You will be unable to
-                  manage your project users during this time. See{' '}
-                  <a
-                    href="https://tacc.utexas.edu/news/user-updates/107601/"
-                    rel="noreferrer"
-                    target="_blank"
-                  >
-                    tacc.utexas.edu/news/user-updates/107601
-                  </a>
-                  .
-                </SectionMessage>
-              )}
-              <SectionHeader actions={<NewProject />} isNestedHeader>
-                Projects & Allocations
-              </SectionHeader>
-            </>
+            <SectionHeader actions={<NewProject />} isNestedHeader>
+              Projects & Allocations
+            </SectionHeader>
           }
           left={<ProjectsNavbar />}
           right={<ProjectsListing />}
