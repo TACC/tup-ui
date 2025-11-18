ba6e13ceb5bae76b6b70a8874cdbc2fce351542f
Wesley B <62723358+wesleyboar@users.noreply.github.com>
2025-11-17 20:22:06 -0600

    fix: bad selector

--- apps/tup-cms/src/taccsite_custom/tup_cms/static/tup_cms/js/ad-hoc/external-articles.js
+++ apps/tup-cms/src/taccsite_custom/tup_cms/static/tup_cms/js/ad-hoc/external-articles.js
@@ -3,7 +3,8 @@ import {
   redirectExternalArticle
 } from 'https://cdn.jsdelivr.net/gh/TACC/Core-CMS@344c33c/taccsite_cms/static/site_cms/js/modules/manageExternalArticles.js';
 
-const isNewsArticle = document.querySelectorAll('.app-blog .post-detail').length;
+/* https://github.com/TACC/Core-CMS/blob/v4.35.13/taccsite_cms/static/site_cms/css/src/_imports/components/django.cms.blog.selectors.css#L6-L7 */
+const isNewsArticle = document.querySelectorAll('.app-blog article.post-detail').length;
 const isNewsList = document.querySelectorAll('.app-blog .blog-list').length;
 
 if (isNewsList) changeHowExternalArticleOpens();
