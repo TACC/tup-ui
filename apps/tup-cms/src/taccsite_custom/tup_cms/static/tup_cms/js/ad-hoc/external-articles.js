b0137390a39d75c5e72025b2db990fab45597b1f
Wesley B <62723358+wesleyboar@users.noreply.github.com>
2025-11-17 20:36:20 -0600

    chore: npx nx format

--- apps/tup-cms/src/taccsite_custom/tup_cms/static/tup_cms/js/ad-hoc/external-articles.js
+++ apps/tup-cms/src/taccsite_custom/tup_cms/static/tup_cms/js/ad-hoc/external-articles.js
@@ -1,10 +1,12 @@
 import {
   changeHowExternalArticleOpens,
-  redirectExternalArticle
+  redirectExternalArticle,
 } from 'https://cdn.jsdelivr.net/gh/TACC/Core-CMS@344c33c/taccsite_cms/static/site_cms/js/modules/manageExternalArticles.js';
 
 /* https://github.com/TACC/Core-CMS/blob/v4.35.13/taccsite_cms/static/site_cms/css/src/_imports/components/django.cms.blog.selectors.css#L6-L7 */
-const isNewsArticle = document.querySelectorAll('.app-blog article.post-detail').length;
+const isNewsArticle = document.querySelectorAll(
+  '.app-blog article.post-detail'
+).length;
 const isNewsList = document.querySelectorAll('.app-blog .blog-list').length;
 
 if (isNewsList) changeHowExternalArticleOpens();
