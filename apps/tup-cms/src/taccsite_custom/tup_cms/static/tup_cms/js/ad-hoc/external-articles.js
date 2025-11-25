ea5683c68be6b70513af3511b323fe45f361a677
Wesley B <62723358+wesleyboar@users.noreply.github.com>
2025-11-25 14:00:10 -0600

    fix: irrelevant warning on non-external articles

--- apps/tup-cms/src/taccsite_custom/tup_cms/static/tup_cms/js/ad-hoc/external-articles.js
+++ apps/tup-cms/src/taccsite_custom/tup_cms/static/tup_cms/js/ad-hoc/external-articles.js
@@ -1,7 +1,7 @@
 import {
   changeHowExternalArticleOpens,
   redirectExternalArticle,
-} from 'https://cdn.jsdelivr.net/gh/TACC/Core-CMS@dd7a5ee/taccsite_cms/static/site_cms/js/modules/manageExternalArticles.js';
+} from 'https://cdn.jsdelivr.net/gh/TACC/Core-CMS@ecd6b18/taccsite_cms/static/site_cms/js/modules/manageExternalArticles.js';
 
 /* https://github.com/TACC/Core-CMS/blob/v4.35.13/taccsite_cms/static/site_cms/css/src/_imports/components/django.cms.blog.selectors.css#L6-L7 */
 const isNewsArticle = document.querySelectorAll(
