011387314d2507c54c0e9d8dbd738153fc7c9af6
Wesley B <62723358+wesleyboar@users.noreply.github.com>
2026-03-03 19:20:53 -0600

    enhance: software table accessibility

--- libs/tup-components/src/software/SoftwareTable.tsx
+++ libs/tup-components/src/software/SoftwareTable.tsx
@@ -151,7 +151,15 @@ const SoftwareTable: React.FC = () => {
   }
   return (
     <>
-      <form className={styles.form}>
+      <form
+        id="st-form"
+        className={styles.form}
+        aria-describedby="st-form-desc"
+        aria-controls="st-table"
+      >
+        <small id="st-form-desc" className="sr-only">
+          Software results in table update as you type or select filters
+        </small>
         <label className={styles['search-field']}>
           <Icon
             name="search"
@@ -167,6 +175,9 @@ const SoftwareTable: React.FC = () => {
             onChange={(e) => setSearchTerm(e.target.value)}
           />
         </label>
+        <small className={styles['results-count']} aria-live="polite">
+          {filteredData?.length ?? 0} results
+        </small>
         <label className={styles['filter-field']}>
           <span>Topic</span>
           <select onChange={(e) => setFilterTopic(e.target.value)}>
@@ -202,7 +213,11 @@ const SoftwareTable: React.FC = () => {
           </select>
         </label>
       </form>
-      <div className={`${styles['table-wrap']}`}>
+      <section
+        id="st-table"
+        className={`${styles['table-wrap']}`}
+        aria-label="Software results"
+      >
         <table className="o-fixed-header-table">
           <thead>
             <tr>
@@ -234,7 +249,7 @@ const SoftwareTable: React.FC = () => {
             ))}
           </tbody>
         </table>
-      </div>
+      </section>
     </>
   );
 };
