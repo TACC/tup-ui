9db864e5144f8118c7f65b823a05f63023ed783a
Wesley B <62723358+wesleyboar@users.noreply.github.com>
2026-03-10 20:08:12 -0500

    Revert "debug: portal nav logged in state not shown"
    This reverts commit 683633e7d16bfb0e9904170778aa21e47c1151b0.

--- apps/tup-cms/src/apps/portal_nav/views.py
+++ apps/tup-cms/src/apps/portal_nav/views.py
@@ -1,24 +1,10 @@
-import logging
-
 from django.http import HttpResponse
 from django.template import loader, Context
 from django.contrib.auth import authenticate
 
-logger = logging.getLogger(f"portal.{__name__}")
-
 
 def PortalNavView(request):
-    auth_user = authenticate(request)
-    # Debug: see plan debug_portal_nav_auth_51488d45
-    logger.info(
-        'portal_nav: request.user=%s is_authenticated=%s authenticate()=%s cookies sessionid=%s x-tup-token=%s',
-        getattr(request.user, 'username', request.user),
-        getattr(request.user, 'is_authenticated', '?'),
-        auth_user,
-        'sessionid' in request.COOKIES,
-        'x-tup-token' in request.COOKIES,
-    )
-    user = auth_user
+    user = authenticate(request)
     is_impersonator = False
     if user:
         is_impersonator = user.groups.filter(name='Impersonator').exists()
